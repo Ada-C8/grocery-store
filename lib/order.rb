b06c2f25c38aed12fda61e1e1373ab9357f4a1bb
Canaan West <canaan.west@gmail.com>
2017-08-22 14:11:24 -0700

    Oops, I forgot to send updates along the way, sorrygit add lib/order.rbgit add lib/order.rbgit add lib/order.rbgit add lib/order.rb

--- lib/order.rb
+++ lib/order.rb
@@ -2,17 +2,35 @@ module Grocery
   class Order
     attr_reader :id, :products
 
-    def initialize(id, products)
+    def initialize(id, collection)
       @id = id
-      @products = products
+      @products = collection
     end
 
     def total
-      # TODO: implement total
+      cost = 0
+      @products.each do |item, price|
+        cost += @products[item]
+      end
+      return (cost * 1.075).round(2)
     end
 
     def add_product(product_name, product_price)
-      # TODO: implement add_product
+      if @products.include?(product_name)
+        return false
+      else
+        @products[product_name] = product_price
+        return true
+      end
+    end
+
+    def remove_product(name)
+      if @products.include?(name)
+        @products[name].delete
+        return true
+      else
+        return false
+      end
     end
   end
 end

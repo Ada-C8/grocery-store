c9d723346f63119d8fb4df537a750be7ae1e6e42
Diane Lenertz <lenertzd@grinnell.edu>
2017-08-22 14:50:35 -0700

    complete Wave 1

--- lib/order.rb
+++ lib/order.rb
@@ -5,14 +5,37 @@ module Grocery
     def initialize(id, products)
       @id = id
       @products = products
+      @original_length = @products.length
+      puts @products
     end
 
     def total
-      # TODO: implement total
+      #: implement total
+      @total = 0
+      @products.each do |product, price|
+        @total += price + price*(0.075)
+      end
+      return @total.round(2)
     end
 
     def add_product(product_name, product_price)
-      # TODO: implement add_product
+      #  implement add_product
+      @products.each do |product, price|
+        if product == product_name
+          return false
+        end
+      end
+      @products[product_name] = product_price
+      return true
+    end
+
+    def remove_product(product_name)
+      @products.delete(product_name)
+      if products.length == @original_length
+        return false
+      else
+        return true
+      end
     end
   end
 end

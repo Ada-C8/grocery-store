df1d354d3cda303f483b42b6d1a6f10416459b48
Eva Sawyer <esawyer25@gmail.com>
2017-08-22 12:17:36 -0700

    made the method total

--- lib/order.rb
+++ lib/order.rb
@@ -9,10 +9,31 @@ module Grocery
 
     def total
       # TODO: implement total
+      total = 0
+      products.each do |name, price|
+        total += price + (price *0.075)
+      end
+       return total.round(2)
     end
+
 
     def add_product(product_name, product_price)
       # TODO: implement add_product
     end
   end
 end
+
+
+#
+#
+# inventory = {
+#   "apple" => 1.0,
+#   "tomato_soup" => 2.0,
+#   "milk" => 2.50,
+# "chicken" => 5.75
+# }
+#
+# t = Grocery::Order.new(1, inventory)
+# puts t.total
+#
+# puts t.products

a80cfe76ac87258111b08742a95dcd8c3129df78
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2017-08-22 13:44:40 -0700

    write block to pass attributes into @products hash & define total method

--- lib/order.rb
+++ lib/order.rb
@@ -3,16 +3,26 @@ module Grocery
     attr_reader :id, :products
 
     def initialize(id, products)
-      @id = id.to_i
-      @products = products.to_sym
+      @id = id
+      @products = {}
+
+      if products.length > 0
+        products.each {|product, value| @products[product] = value}
+      end
+
     end
 
     def total
-      # TODO: implement total
+      sum = 0
+      products.each_value do |value|
+        sum = sum + value
+      end
+      expected_total = sum + (sum * 0.075).round(2)
+      return expected_total
     end
 
     def add_product(product_name, product_price)
       # TODO: implement add_product
     end
-
   end
+end

7084ad8c410363307c201a27ed08d43d92bb076b
nkiru onwuneme <generalhrh@gmail.com>
2017-08-22 15:58:07 -0700

    Added code for second test

--- lib/order.rb
+++ lib/order.rb
@@ -1,5 +1,4 @@
 module Grocery
-  PRODUCT = {tomato: 3.49, pear: 2.49, rice: 1.50, beans: 4.00}
   ### Don't forget to commit
   class Order
     attr_reader :id, :products
@@ -11,20 +10,24 @@ module Grocery
       @products.length == 0
     end
 
-    # def total
-    #   sum = 0
-    #   # expected_total = 0
-    #   # products.each do|item,cost|
-    #   # sum = products.cost.map(&:last).reduce(:+)
-    #   # @products.inject(0) |item, cost|
-    #   #   sum += cost.last
-    #
-    #     #inject(0) is the initial value of total.
-    #   # expected_total = sum + (sum * 0.075).round(2)
-    #   products.each do |item, cost|
-    #     sum += value.last.to_f
-    #     # expected_total = sum + (sum * 0.075).round(2)
-    #   end
+    def total
+      sum = 0
+      products.each do |item, cost|
+        sum = (sum + cost)
+      end
+        total = ((sum * 0.075) + sum).round(2)
+
+      # expected_total = 0
+      # products.each do|item,cost|
+      # sum = products.cost.map(&:last).reduce(:+)
+      # @products.inject(0) |item, cost|
+      #   sum += cost.last
+
+      #inject(0) is the initial value of total.
+      # expected_total = sum + (sum * 0.075).round(2)
+
+      # expected_total = sum + (sum * 0.075).round(2)
+    end
 
 
 
@@ -34,5 +37,5 @@ module Grocery
     #   def add_product(product_name, product_price)
     #     # TODO: implement add_product
     #   end
-  end
-end
+  end # end of class
+end # module

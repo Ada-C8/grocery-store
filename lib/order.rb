c3d165bc6fa00da8590bc2c9f444877103e85dd0
Jocelyn Gonzalez <jocegonz@gmail.com>
2017-08-22 14:58:40 -0700

    add describe total

--- lib/order.rb
+++ lib/order.rb
@@ -3,17 +3,18 @@ module Grocery
     attr_reader :id, :products
 
     def initialize(id, products)
-
-      # until id.is_a? Integer
-      #   raise ArgumentError.new("id is not an integer")
-      # end
-
       @id = id
       @products = products
     end
 
     def total
       # TODO: implement total
+
+      @total = @products.values.inject(0, :+)
+
+      @total_plus_tax = @total + (@total * 0.075).round(2)
+
+      return @total_plus_tax
     end
 
     def add_product(product_name, product_price)

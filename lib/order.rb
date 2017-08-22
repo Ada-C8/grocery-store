00ee2b29cb148847329a38197dba2e994b403255
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-08-22 12:20:47 -0700

    implement Order.total method

--- lib/order.rb
+++ lib/order.rb
@@ -8,11 +8,21 @@ module Grocery
     end
 
     def total
-      # TODO: implement total
+      total = 0
+      @products.each do |product|
+        total += product[1]
+      end
+      total = (total * 1.075).round(2)
+      return total
     end
 
-    def add_product(product_name, product_price)
-      # TODO: implement add_product
-    end
+    # def add_product(product_name, product_price)
+    #   unless @products.has_key?(product_name)
+    #     @products[product_name] = product_price
+    #     return true
+    #   else
+    #     return false
+    #   end
+    # end
   end
 end

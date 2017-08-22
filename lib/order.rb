119cf437485475408e15dc1a1b835d5bc276e15e
Shaunna <skwiens@gmail.com>
2017-08-22 14:11:29 -0700

    Wave1 requirements met. methods: remove_product, add_product, total

--- lib/order.rb
+++ lib/order.rb
@@ -8,11 +8,35 @@ module Grocery
     end
 
     def total
-      # TODO: implement total
+      sum = 0
+      products.each do |product, price|
+        sum += price
+      end
+      total = (sum + sum * 0.075).round(2)
+      return total
     end
 
     def add_product(product_name, product_price)
-      # TODO: implement add_product
+      if @products.keys.include?(product_name)
+        return false
+      else
+        @products[product_name] = product_price
+        return true
+      end
+    end
+
+    def remove_product(product_name)
+      if @products.keys.include?(product_name)
+        @products.delete(product_name)
+        return true
+      else
+        return false
+      end 
     end
   end
 end
+
+# order = Grocery::Order.new(2314, {"banana" => 1.99, "cracker" => 3.00})
+#
+# order.add_product("salad", 4.25)
+# p order.products

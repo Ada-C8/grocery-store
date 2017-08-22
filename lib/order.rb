adf60f68a9e57fcda814a141de28c0e508502aab
IsabelDePapel <isuchanek@gmail.com>
2017-08-22 13:25:16 -0700

    Implemented total method

--- lib/order.rb
+++ lib/order.rb
@@ -3,12 +3,22 @@ module Grocery
     attr_reader :id, :products
 
     def initialize(id, products)
+      # products if a hash with product name as key, price as value
       @id = id
       @products = products
     end
 
     def total
       # TODO: implement total
+      total = 0
+
+      # iterate through products hash and add to total
+      products.each do |product_name, product_price|
+        total += product_price
+      end
+
+      # add 7.5% tax and round to 2 decimal places
+      return (total * 1.075).round(2)
     end
 
     def add_product(product_name, product_price)

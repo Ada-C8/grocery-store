26a72510df71270cf7e3658cca4102ea014d8bde
Bennett Rahn <rahnibennett@gmail.com>
2017-08-22 15:59:09 -0700

    change to include product class

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,5 @@
+# new code trying out products as classes
+require_relative 'product'
 
 module Grocery
   class Order
@@ -35,3 +37,41 @@ module Grocery
     end
   end
 end
+
+# # og code using hash products
+# module Grocery
+#   class Order
+#     attr_reader :id, :products
+#
+#     def initialize(id, products)
+#       @id = id
+#       @products = products
+#     end
+#
+#     def total
+#       # TODO: implement total
+#       subtotal = @products.values.inject(0, :+)
+#       total = (subtotal * 1.075).round(2)
+#       return total
+#     end
+#
+#     def add_product(product_name, product_price)
+#       # TODO: implement add_product
+#       if @products.keys.include?(product_name)
+#         return false
+#       else
+#         @products[product_name] = product_price
+#         return true
+#       end
+#     end
+#     def remove_product(product_name)
+#       if @products.keys.include?(product_name)
+#         @products.delete(product_name)
+#         return true
+#       else
+#         return false
+#       end
+#
+#     end
+#   end
+# end

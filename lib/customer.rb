57f08b075ed890aa959ce67b2ccd3b64c6c6a671
averikitsch <aekitsch@gmail.com>
2017-08-22 16:38:00 -0700

    Added customer class and tests

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,14 +1,41 @@
 module Grocery
   class Customer
-    attr_reader
+    attr_reader :id, :email, :address
 
-    def initialize
+    def initialize(id,email,address)
       @id = id
       @email = email
       @address = address
     end
 
+    def self.all(csv_file)
+      @@array_of_customers = []
+      csv_file.length.times do |i|
+        csv_line = csv_file[i]
+        id = csv_line[0].to_i
+        email = csv_line[1]
+        address = {}
+        address[:address_line] = csv_line[2]
+        address[:city] = csv_line[3]
+        address[:state] = csv_line[4]
+        address[:zip_code] = csv_line[5]
+        @@array_of_customers << self.new(id, email,address)
+      end
+      return @@array_of_customers
+    end
 
+    def self.find(id_lookup)
+      customer_to_return = nil
+      @@array_of_customers.each do |customer|
+        if customer.id == id_lookup
+          customer_to_return = order
+        end
+      end
+      if customer_to_return.nil?
+        raise ArgumentError.new "No customer with that Id #"
+      end
+      return customer_to_return
+    end
 
   end # end class
 end #end module
--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -1,18 +1,40 @@
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
-
+require 'csv'
 # TODO: uncomment the next line once you start wave 3
-# require_relative '../lib/customer'
+ require_relative '../lib/customer'
 
-xdescribe "Customer" do
+describe "Customer" do
+  before do
+    customer_csv = CSV.read("/Users/averikitsch/ada/week-03/grocery-store/support/customers.csv")
+    @customer_array = Grocery::Customer.all(customer_csv)
+  end
   describe "#initialize" do
     it "Takes an ID, email and address info" do
-      # TODO: Your test code here!
+      id = 101
+      email = "aekitsch@gmail.com"
+      address = {
+        address_line: "3932 S 284th Pl",
+        city: "Auburn",
+        state: "WA",
+        zip_code: "98001"
+      }
+      customer = Grocery::Customer.new(id,email,address)
+
+      customer.must_respond_to :id
+      customer.id.must_equal id
+      customer.id.must_be_kind_of Integer
+
+      customer.must_respond_to :email
+      customer.email.must_equal email
+
+      customer.must_respond_to :address
+      customer.address.must_be_kind_of Hash
     end
   end
 
-  describe "Customer.all" do
+  xdescribe "Customer.all" do
     it "Returns an array of all customers" do
       # TODO: Your test code here!
       # Useful checks might include:
@@ -22,10 +44,14 @@ xdescribe "Customer" do
       #   - The ID, email address of the first and last
       #       customer match what's in the CSV file
       # Feel free to split this into multiple tests if needed
+      @customer_array.must_be_kind_of Array
+      @customer_array.each do |i|
+        i.class.must_be_instance_of Grocery::Customer
+      end
     end
   end
 
-  describe "Customer.find" do
+  xdescribe "Customer.find" do
     it "Can find the first customer from the CSV" do
       # TODO: Your test code here!
     end

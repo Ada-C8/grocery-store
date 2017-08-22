e2e4986d3941205a2da72d79ca4b8bae21e218fb
Jocelyn Gonzalez <jocegonz@gmail.com>
2017-08-22 15:27:51 -0700

    describe add product it- return false if product exists

--- lib/order.rb
+++ lib/order.rb
@@ -9,16 +9,20 @@ module Grocery
 
     def total
       # TODO: implement total
-
       @total = @products.values.inject(0, :+)
-
       @total_plus_tax = @total + (@total * 0.075).round(2)
-
       return @total_plus_tax
     end
 
     def add_product(product_name, product_price)
       # TODO: implement add_product
+      @product_name = product_name
+      @product_price = product_price
+      @products.each do |key, value|
+        if key == @product_name
+          return false #product exists
+        end
+      end
     end
   end
 end

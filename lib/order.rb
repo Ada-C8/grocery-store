a7bd3f83beb9ec7775b119cb851cbea4b60dbb2e
Roxanne <argonkryptonneon@gmail.com>
2017-08-22 14:58:17 -0700

    i was afraid of commitment until now.  Thanks, Ruby.

--- lib/order.rb
+++ lib/order.rb
@@ -9,10 +9,20 @@ module Grocery
 
     def total
       # TODO: implement total
+      total = 0
+      products.each {|name, price| total += price}
+      total = total + (total* 0.075).round(2)
     end
 
     def add_product(product_name, product_price)
       # TODO: implement add_product
+      if !@products.has_key? product_name
+        @products[product_name] = product_price
+        true
+      else
+        false
+      end
     end
   end
+
 end

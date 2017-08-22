aefdd87683a1aa362a7a7a528c38baef2de4c79f
nkiru onwuneme <generalhrh@gmail.com>
2017-08-22 15:01:18 -0700

    Added code to order.rb for first test

--- lib/order.rb
+++ lib/order.rb
@@ -1,18 +1,38 @@
 module Grocery
+  PRODUCT = {tomato: 3.49, pear: 2.49, rice: 1.50, beans: 4.00}
+  ### Don't forget to commit
   class Order
     attr_reader :id, :products
 
     def initialize(id, products)
       @id = id
+      @id.is_a? Integer
       @products = products
+      @products.length == 0
     end
 
-    def total
-      # TODO: implement total
-    end
+    # def total
+    #   sum = 0
+    #   # expected_total = 0
+    #   # products.each do|item,cost|
+    #   # sum = products.cost.map(&:last).reduce(:+)
+    #   # @products.inject(0) |item, cost|
+    #   #   sum += cost.last
+    #
+    #     #inject(0) is the initial value of total.
+    #   # expected_total = sum + (sum * 0.075).round(2)
+    #   products.each do |item, cost|
+    #     sum += value.last.to_f
+    #     # expected_total = sum + (sum * 0.075).round(2)
+    #   end
 
-    def add_product(product_name, product_price)
-      # TODO: implement add_product
-    end
+
+
+    #   end
+    #
+    #
+    #   def add_product(product_name, product_price)
+    #     # TODO: implement add_product
+    #   end
   end
 end

624e2aff9915bbb31575196a62a7cbe6176f3cc1
Irene DeVera <irenneeyyy@gmail.com>
2017-08-22 15:55:08 -0700

    First commit, add methods to order.rb

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,27 @@
+# Create a class inside of a module
+# Create methods inside the class to perform actions
+# Learn how Ruby does error handling
+# Verify code correctness by testing
+
+# For Wave 1, all tests have been provided for you. For each piece of functionality that you build, you should run the tests from the command line using the rake command. To focus on only one test at a time, change all it methods to xit except for the one test you'd like to run. All tests provided should be passing at the end of your work on Wave 1.
+#
+# Requirements
+#
+# Create a Grocery module which will contain an Order class and any future grocery store logic.
+#
+# Create an Order class which should have the following functionality:
+
+# an ID, read-only
+# a collection of products and their cost
+# zero products is permitted
+# you can assume that there is only one of each product
+# A total method which will calculate the total cost of the order by:
+# summing up the products
+# adding a 7.5% tax
+# ensure the result is rounded to two decimal places
+# An add_product method which will take in two parameters, product name and price, and add the data to the product collection
+# It should return true if the item was successfully added and false if it was not
+
 module Grocery
   class Order
     attr_reader :id, :products
@@ -8,11 +32,33 @@ module Grocery
     end
 
     def total
-      # TODO: implement total
+      if @products.length == 0
+        return 0
+      else
+        sum = @products.values.inject(0, :+)
+        expected_total = sum + (sum * 0.075).round(2)
+        return expected_total
+      end
     end
 
     def add_product(product_name, product_price)
-      # TODO: implement add_product
-    end
-  end
-end
+      # EVALUATE TO SEE IF PRODUCT NAME IS ALREADY IN THE LIST
+      if @products.include?(product_name)
+        return false # And do not add to the list and end method
+      else
+        @products[product_name] = product_price # Add to hash
+        if @products.include?(product_name) # Tests if it was added and if it was new
+          return true
+        end
+      end
+    end # DEF
+
+    def remove_product(product_name)
+      @products.delete(product_name)
+      if @products.include?(product_name)
+        return false
+      end
+    end # DEF
+
+  end # CLASS
+end # MODULE
--- specs/order_spec.rb
+++ specs/order_spec.rb
@@ -76,12 +76,32 @@ describe "Order Wave 1" do
       result.must_equal true
     end
   end
+
+  describe "#remove_product" do
+    it "Decreases the number of products by one" do
+      products = { "banana" => 1.99, "cracker" => 3.00 }
+      before_count = products.count
+      order = Grocery::Order.new(1337, products)
+
+      order.remove_product("banana")
+      expected_count = before_count - 1
+      order.products.count.must_equal expected_count
+    end
+
+    it "Is removed from the collection of products" do
+      products = { "banana" => 1.99, "cracker" => 3.00 }
+      order = Grocery::Order.new(1337, products)
+
+      order.remove_product("banana")
+      order.products.include?("banana").must_equal false
+    end
+  end
 end
 
 # TODO: change 'xdescribe' to 'describe' to run these tests
 xdescribe "Order Wave 2" do
   describe "Order.all" do
-    it "Returns an array of all orders" do
+    xit "Returns an array of all orders" do
       # TODO: Your test code here!
       # Useful checks might include:
       #   - Order.all returns an array
@@ -94,15 +114,15 @@ xdescribe "Order Wave 2" do
   end
 
   describe "Order.find" do
-    it "Can find the first order from the CSV" do
+    xit "Can find the first order from the CSV" do
       # TODO: Your test code here!
     end
 
-    it "Can find the last order from the CSV" do
+    xit "Can find the last order from the CSV" do
       # TODO: Your test code here!
     end
 
-    it "Raises an error for an order that doesn't exist" do
+    xit "Raises an error for an order that doesn't exist" do
       # TODO: Your test code here!
     end
   end

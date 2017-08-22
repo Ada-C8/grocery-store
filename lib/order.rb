741b75fd58c4b926d511c0ff06032ed1847352fa
nkiru onwuneme <generalhrh@gmail.com>
2017-08-22 16:25:26 -0700

    Added code to order.rb for test #3

--- lib/order.rb
+++ lib/order.rb
@@ -15,27 +15,14 @@ module Grocery
       products.each do |item, cost|
         sum = (sum + cost)
       end
-        total = ((sum * 0.075) + sum).round(2)
+      total = ((sum * 0.075) + sum).round(2)
+    end
 
-      # expected_total = 0
-      # products.each do|item,cost|
-      # sum = products.cost.map(&:last).reduce(:+)
-      # @products.inject(0) |item, cost|
-      #   sum += cost.last
-
-      #inject(0) is the initial value of total.
-      # expected_total = sum + (sum * 0.075).round(2)
-
-      # expected_total = sum + (sum * 0.075).round(2)
+    def add_product(product_name, product_price)
+      @products[product_name] = product_price
     end
 
 
 
-    #   end
-    #
-    #
-    #   def add_product(product_name, product_price)
-    #     # TODO: implement add_product
-    #   end
   end # end of class
 end # module

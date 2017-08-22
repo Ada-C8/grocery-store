15b1f6f4e49bf0ad7105692976d92f3e274dbffa
Amy Lee <amy.yj.lee@gmail.com>
2017-08-22 14:08:53 -0700

    Added tests for the #remove_product method

--- specs/order_spec.rb
+++ specs/order_spec.rb
@@ -75,7 +75,53 @@ describe "Order Wave 1" do
       result = order.add_product("salad", 4.25)
       result.must_equal true
     end
-  end
+  end #end add_products tests
+
+  describe "#remove_product" do
+    it "Decreases the number of products" do
+      products = { "banana" => 1.99, "cracker" => 3.00 }
+      before_count = products.count
+      order = Grocery::Order.new(1337, products)
+
+      order.remove_product("cracker")
+      expected_count = before_count - 1
+      order.products.count.must_equal expected_count
+    end
+
+    it "Is removed from the collection of products" do
+      products = { "banana" => 1.99, "cracker" => 3.00, "sandwich" => 4.25 }
+      order = Grocery::Order.new(1337, products)
+
+      order.remove_product("banana")
+      order.products.include?("banana").must_equal false
+    end
+
+    it "Returns false if the product is not removed" do
+      products = { "banana" => 1.99, "cracker" => 3.00, "sandwich" => 4.25 }
+
+      order = Grocery::Order.new(1337, products)
+      before_total = order.total
+
+      result = order.remove_product("nutella")
+      after_total = order.total
+
+      result.must_equal false
+      before_total.must_equal after_total
+    end
+
+    it "Returns true if the product is removed" do
+      products = { "banana" => 1.99, "cracker" => 3.00, "sandwich" => 4.25 }
+      order = Grocery::Order.new(1337, products)
+      before_total = order.total
+
+      result = order.remove_product("cracker")
+      after_total = order.total
+
+      result.must_equal true
+      before_total.must_be :>, after_total
+    end
+  end #end remove_products tests
+
 end
 
 # TODO: change 'xdescribe' to 'describe' to run these tests

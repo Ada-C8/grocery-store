39a0ab1e84cb935e5f484043e3eb59c57500b30c
Amy Lee <amy.yj.lee@gmail.com>
2017-08-22 13:35:30 -0700

    Defined a working method total for the Order Class

--- lib/order.rb
+++ lib/order.rb
@@ -1,14 +1,26 @@
 module Grocery
   class Order
     attr_reader :id, :products
+    TAX = 0.075
 
     def initialize(id, products)
       @id = id
       @products = products
+      @total = 0
     end
 
     def total
       # TODO: implement total
+      pretax_total = 0
+
+      @products.each do |product, price|
+        pretax_total += price
+      end
+
+      @total = pretax_total + (pretax_total * 0.075).round(2)
+
+      return @total
+
     end
 
     def add_product(product_name, product_price)

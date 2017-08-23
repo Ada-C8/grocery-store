68a49143e2a5674f6c854bb40b2fe9eb23c4c2fe
Bennett Rahn <rahnibennett@gmail.com>
2017-08-23 15:40:38 -0700

    add all method

--- lib/order.rb
+++ lib/order.rb
@@ -1,6 +1,6 @@
-# new code trying out products as classes
-require_relative 'product'
 
+
+# og code using hash products
 module Grocery
   class Order
     attr_reader :id, :products
@@ -26,6 +26,7 @@ module Grocery
         return true
       end
     end
+
     def remove_product(product_name)
       if @products.keys.include?(product_name)
         @products.delete(product_name)
@@ -33,45 +34,10 @@ module Grocery
       else
         return false
       end
+    end
+
+    def self.all
 
     end
   end
 end
-
-# # og code using hash products
-# module Grocery
-#   class Order
-#     attr_reader :id, :products
-#
-#     def initialize(id, products)
-#       @id = id
-#       @products = products
-#     end
-#
-#     def total
-#       # TODO: implement total
-#       subtotal = @products.values.inject(0, :+)
-#       total = (subtotal * 1.075).round(2)
-#       return total
-#     end
-#
-#     def add_product(product_name, product_price)
-#       # TODO: implement add_product
-#       if @products.keys.include?(product_name)
-#         return false
-#       else
-#         @products[product_name] = product_price
-#         return true
-#       end
-#     end
-#     def remove_product(product_name)
-#       if @products.keys.include?(product_name)
-#         @products.delete(product_name)
-#         return true
-#       else
-#         return false
-#       end
-#
-#     end
-#   end
-# end

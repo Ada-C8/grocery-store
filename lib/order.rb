3ee532601fb3668cd1bfb2de3fc30574b816d0e6
Eva Sawyer <esawyer25@gmail.com>
2017-08-23 16:49:52 -0700

    added a Grocery_Records class that can read the csv file

--- lib/order.rb
+++ lib/order.rb
@@ -1,4 +1,57 @@
+require 'csv'
+
 module Grocery
+
+  class Grocery_Records
+    attr_reader
+
+    def initialize
+      @file_name = ""
+    end
+
+    def read_csv(file_name)
+      array_of_all_orders =[]
+      CSV.open(file_name, 'r').each do |line|
+        #divide the cvs file into the order number, and the products
+        order_id = line[0]
+        order_contents = line[1]
+        order_contents = order_contents.split(";")
+        order_contents.to_s.delete("\,")
+        order_contents.to_s.delete(" \" ")
+
+
+        products = []
+        item_index = 0
+        order_contents.each do |items|
+          current_item = order_contents[item_index]
+          item_name = current_item.to_s.split("\:")[0]
+          item_cost = current_item.to_s.split("\:")[1].to_f
+          item_index = item_index + 1
+
+          #make a hash for one product (item_name => item_cost)
+          current_item = Hash.new
+          current_item = {
+            item_name => item_cost
+          }
+          #make an array of hashes for all items in order
+          #ex. ({name1 => cost 1}, {name2 => cost 2})
+          products << current_item
+          products
+        end
+        # make an array with the order id and the products
+        #ex. (order id, ({name1 => cost 1}, {name2 => cost 2}))
+        current_order = [order_id, products]
+        #make an array of all the orders
+        array_of_all_orders << current_order
+      end
+      array_of_all_orders
+    end
+
+
+
+  end #end of class
+
+
   class Order
     attr_reader :id, :products
 
@@ -14,6 +67,16 @@ module Grocery
       end
       return total.round(2)
     end
+
+    # def update_database(file_name)
+    #   new_database = Grocery_Records.new
+    #   new_database.read_csv(file_name)
+    #
+    #   new_database.each do |order|
+    #     Order
+    # end
+
+
 
     def add_product(product_name, product_price)
       if @products.key?(product_name)
@@ -36,7 +99,10 @@ module Grocery
   end #end  class Order
 end #end module Grocery
 #
-#
+t = Grocery::Order.new(1, {"Slivered Almonds"=>22.88, "Wholewheat flour"=>1.93, "Grape Seed Oil"=>74.9})
+
+puts t.total
+
 # inventory = {
 #   "apple" => 1.0,
 #   "tomato_soup" => 2.0,

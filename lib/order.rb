17865068161c5e0cfa537fd3453a88e244e9bbeb
Shaunna <skwiens@gmail.com>
2017-08-23 15:31:14 -0700

    order.find written

--- lib/order.rb
+++ lib/order.rb
@@ -3,29 +3,42 @@ require 'awesome_print'
 
 module Grocery
   class Order
+    @@orders = []
     attr_reader :id, :products
 
     def initialize(id, products)
       @id = id
       @products = products
+      @@orders << self
     end
 
     def self.all(csv_file)
-      @@orders = []
+      csv_orders = Array.new
       CSV.read(csv_file).each do |purchase_array|
         order_id = purchase_array[0].to_i
         products_list = purchase_array[1].split(";")
+        products = {}
         products_list.each do |product|
-          products = {}
           product_with_price = product.split(":")
-          products[product_with_price[0]] = product_with_price[1].to_i
-          @@orders << Grocery::Order.new(order_id, products)
+          products[product_with_price[0]] = product_with_price[1].to_f
         end
+        csv_orders << Grocery::Order.new(order_id, products)
       end
-      return @@orders
+      return csv_orders
     end
 
-    def self.find_id(id)
+    def self.find_id(csv_file, id)
+      csv_orders = self.all(csv_file)
+      csv_orders.each do |order|
+        if order.id == id
+          return order
+        end
+      end
+      raise ArgumentError.new "This order doesn't exist"
+    end
+
+    def self.show_all_orders
+      return @@orders
     end
 
     def total
@@ -58,7 +71,14 @@ module Grocery
   end
 end
 
-ap Grocery::Order.all('./support/orders.csv')
+orders = Grocery::Order.all('./support/orders.csv')
+
+p Grocery::Order.find_id('./support/orders.csv', 54)
+#
+# ap Grocery::Order.show_all_orders
+
+
+
 # order = Grocery::Order.new(2314, {"banana" => 1.99, "cracker" => 3.00})
 #
 # order.add_product("salad", 4.25)

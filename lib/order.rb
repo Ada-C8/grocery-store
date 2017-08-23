d52e40e55613f4c864ab26a29f7c88c9a40b54dd
enigmagnetic <lauren.cardella@gmail.com>
2017-08-23 15:24:17 -0700

    Order can ready in order objects from CSV file.

--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 module Grocery
   class Order
     attr_reader :id, :products
@@ -5,6 +7,34 @@ module Grocery
     def initialize(id, products)
       @id = id
       @products = products
+    end
+
+    def self.all
+      orders = []
+      CSV.open('./support/orders.csv', "r", headers: true).each do |row|
+        order_products = row["products"].split(";")
+        products = {}
+        order_products.each do |product_price|
+          product_hash = product_price.split(":")
+          products.store(product_hash[0], product_hash[1].to_f)
+        end
+        orders << Order.new(row["id"].to_i, products)
+      end
+      orders
+    end
+
+      # `self.find(id)` - returns an instance of `Order` where the value of the id field in the CSV matches the passed parameter.
+
+    def self.find(id_num)
+      orders = Order.all
+      ids = []
+      orders.each do |order|
+        ids << order.id
+        return order if order.id == id_num
+      end
+      if !(ids.include?(id_num))
+        return "Invalid Order Number"
+      end
     end
 
     def total
@@ -34,3 +64,15 @@ module Grocery
     end
   end
 end
+
+# CSV.open('../support/orders.csv', "r", headers: true).each do |row|
+#   order_products = row["products"].split(";")
+#   products = []
+#   order_products.each do |product_price|
+#     product_hash = product_price.split(":")
+#     products << {product_hash[0] => product_hash[1]}
+#     puts products
+#   end
+#   order = Order.new(row["id"], products)
+#   puts order
+# end

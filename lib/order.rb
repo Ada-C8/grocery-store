40cd51e86cee87afbf8a00653c62782feaf8536c
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2017-08-23 19:41:43 -0700

    Import CSV data and stored in a hash of arrays in hashes

--- lib/order.rb
+++ lib/order.rb
@@ -1,6 +1,29 @@
+require 'csv'
+
+hash = {}
+
+CSV.open("support/orders.csv", "r").each do |line|
+   hash[line[0].to_i] = line[1]
+ end
+
+hash.each do |k, v|
+  hash[k] = v.split(";")
+end
+
+hash.each do |k,v|
+  product_hash = {}
+  v.each do |string|
+    order = string.split(":")
+    product_hash[order[0]] = order[1].to_f
+  end
+  hash[k] = product_hash
+end
+
 module Grocery
   class Order
     attr_reader :id, :products
+
+@@array = []
 
     def initialize(id, products)
       @id = id
@@ -10,6 +33,8 @@ module Grocery
         products.each {|product, value| @products[product] = value}
       end
 
+      item_array = [@id, @products]
+      @@array << item_array
     end
 
     def total
@@ -37,8 +62,26 @@ module Grocery
         return @products
       end
 
+#class method
+#returns a collection of order instances, representing all the orders
+#in the CSV file
+
+    def self.all
+    end
+
+#class method
+    def self.find(id)
+    end
 
     end
 
   end
 end
+
+
+array = []
+hash[1].each_value do |product, value|
+  array << [product] = value
+end
+
+puts array

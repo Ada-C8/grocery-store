394c59e0a2ac3cc6f8139dc7119dddcb274a93ba
Bennett Rahn <rahnibennett@gmail.com>
2017-08-24 13:56:45 -0700

    actually create the customer class within Grocery module

---
+++ lib/customer.rb
@@ -0,0 +1,13 @@
+module Grocery
+  class Customer
+    attr_reader :id, :email, :address
+
+    def initialize(id, email, address)
+      @id = id
+      @email = email
+      @address = address
+    end
+
+
+  end
+end

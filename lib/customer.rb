ecd5a8c74d7e921cf0721b063e52e8fa7d2310c7
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-08-24 14:51:07 -0700

    Add Grocery module outside Customer class in customer.rb

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,34 +1,36 @@
 require 'csv'
 
-class Grocery::Customer
-#class Customer
-  attr_reader :id, :email, :address
+module Grocery
+  class Customer
+  #class Customer
+    attr_reader :id, :email, :address
 
-  @@customers = []
-
-  def initialize(id, email, address)
-    @id = id
-    @email = email
-    @address = address
-    @@customers.push(self)
-  end
-
-  def self.all
-    return @@customers
-  end
-
-  def self.read(filename)
     @@customers = []
-    CSV.foreach(filename) do |row|
-      self.new(row[0].to_i, row[1], row[2..-1].join(","))
-    end
-  end
 
-  def self.find(id)
-    @@customers.each do |customer|
-      return customer if customer.id == id
+    def initialize(id, email, address)
+      @id = id
+      @email = email
+      @address = address
+      @@customers.push(self)
     end
-    raise ArgumentError.new("This customer id does not exist")
-  end
 
+    def self.all
+      return @@customers
+    end
+
+    def self.read(filename)
+      @@customers = []
+      CSV.foreach(filename) do |row|
+        self.new(row[0].to_i, row[1], row[2..-1].join(","))
+      end
+    end
+
+    def self.find(id)
+      @@customers.each do |customer|
+        return customer if customer.id == id
+      end
+      raise ArgumentError.new("This customer id does not exist")
+    end
+
+  end
 end
--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -6,49 +6,49 @@ require_relative '../lib/customer'
 describe "Customer" do
   describe "#initialize" do
     it "Takes an ID, email and address info" do
-      proc {Customer.new}.must_raise ArgumentError
-      Customer.new(12, "test@test.com", "701 Fake Street, Seattle, WA 98102").class.must_equal Customer
+      proc {Grocery::Customer.new}.must_raise ArgumentError
+      Grocery::Customer.new(12, "test@test.com", "701 Fake Street, Seattle, WA 98102").class.must_equal Grocery::Customer
     end
   end
 
-  describe "Customer.all" do
+  describe "Grocery::Customer.all" do
     before do
-      Customer.read(File.expand_path('../..', __FILE__) + "/support/customers.csv")
+      Grocery::Customer.read(File.expand_path('../..', __FILE__) + "/support/customers.csv")
     end
     it "Returns an array of all customers" do
-      Customer.all.class.must_equal Array
-      Customer.all.each do |customer|
-        customer.class.must_equal Customer
+      Grocery::Customer.all.class.must_equal Array
+      Grocery::Customer.all.each do |customer|
+        customer.class.must_equal Grocery::Customer
       end
-      Customer.all.length.must_equal 35
+      Grocery::Customer.all.length.must_equal 35
     end
     it "Returns accurate information about the first customer" do
-      Customer.all[0].id.must_equal 1
-      Customer.all[0].email.must_equal "leonard.rogahn@hagenes.org"
-      Customer.all[0].address.must_equal "71596 Eden Route,Connellymouth,LA,98872-9105"
+      Grocery::Customer.all[0].id.must_equal 1
+      Grocery::Customer.all[0].email.must_equal "leonard.rogahn@hagenes.org"
+      Grocery::Customer.all[0].address.must_equal "71596 Eden Route,Connellymouth,LA,98872-9105"
     end
     it "Returns accurate information about the last customer" do
-      Customer.all[-1].id.must_equal 35
-      Customer.all[-1].email.must_equal "rogers_koelpin@oconnell.org"
-      Customer.all[-1].address.must_equal "7513 Kaylee Summit,Uptonhaven,DE,64529-2614"
+      Grocery::Customer.all[-1].id.must_equal 35
+      Grocery::Customer.all[-1].email.must_equal "rogers_koelpin@oconnell.org"
+      Grocery::Customer.all[-1].address.must_equal "7513 Kaylee Summit,Uptonhaven,DE,64529-2614"
     end
   end
 
-  describe "Customer.find" do
+  describe "Grocery::Customer.find" do
     before do
-      Customer.read(File.expand_path('../..', __FILE__) + "/support/customers.csv")
-      #Customer.read("./support/customers.csv")
+      Grocery::Customer.read(File.expand_path('../..', __FILE__) + "/support/customers.csv")
+      #Grocery::Customer.read("./support/customers.csv")
     end
     it "Can find the first customer from the CSV" do
-      Customer.find(1).email.must_equal "leonard.rogahn@hagenes.org"
+      Grocery::Customer.find(1).email.must_equal "leonard.rogahn@hagenes.org"
     end
 
     it "Can find the last customer from the CSV" do
-      Customer.find(35).email.must_equal "rogers_koelpin@oconnell.org"
+      Grocery::Customer.find(35).email.must_equal "rogers_koelpin@oconnell.org"
     end
 
     it "Raises an error for a customer that doesn't exist" do
-      proc {Customer.find(500)}.must_raise ArgumentError
+      proc {Grocery::Customer.find(500)}.must_raise ArgumentError
     end
   end
 end

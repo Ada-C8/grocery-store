c79ba9d95e0596ed9018c4c755fe574531f54099
Sarah Read-Brown <srbusiness2@gmail.com>
2017-08-24 15:27:11 -0700

    Add barebones module and method def to customer class file

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,2 +1,20 @@
 require 'pry'
 require 'csv'
+module  Grocery
+  class Customer
+
+    def initialize
+
+    end
+
+    def self.all
+      # returns a collection of Customer instances, representing all of the Customer described in the CSV. See below for the CSV file specifications
+
+    end
+
+    def self.find(id)
+      # self.find(id) - returns an instance of Customer where the value of the id field in the CSV matches the passed parameter
+    end
+
+  end
+end

97bdccd9165356f9a78a0927ee548f1ce28846e7
murog <guillermina.muro@gmail.com>
2017-08-24 16:34:25 -0700

    added tests to customer_spec.rb

--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -8,34 +8,74 @@ require 'minitest/skip_dsl'
 xdescribe "Customer" do
   describe "#initialize" do
     it "Takes an ID, email and address info" do
-      # TODO: Your test code here!
+      id = 1337
+      email = "person@gmail.com"
+      address_info = {
+      address1: "123 sesame st",
+      city: "seattle",
+      state: "WA",
+      zip: "123456"}
+      new_customer = Grocery::Customer.new(id, email, address_info)
+
+      new_customer.must_respond_to :id
+      new_customer.must_respond_to :email
+      new_customer.must_respond_to :address_info
+
+      new_customer.id.must_be_kind_of Integer # => does this have to be the case?
+      new_customer.id.must_equal id
+      new_customer.email.must_equal  email
+      new_customer.address_info.must_equal address_info
+
     end
   end
 
   describe "Customer.all" do
-    it "Returns an array of all customers" do
-      # TODO: Your test code here!
-      # Useful checks might include:
-      #   - Customer.all returns an array
-      #   - Everything in the array is a Customer
-      #   - The number of orders is correct
-      #   - The ID, email address of the first and last
-      #       customer match what's in the CSV file
-      # Feel free to split this into multiple tests if needed
+    it "Customer.all returns an array" do
+      result = Grocery::Customer.all
+      result.must_be_instance_of Array
     end
-  end
+    it "Everything in the array is a customer" do
+      result = Grocery::Customer.all
+      result.each do |element|
+        element.must_be_instance_of Customer
+      end
+    end
+    it "The number of order is correct" do
+      result = Grocery::Customer.all
+      result.length.must_equal 35
+    end
+    it "The ID, email address of the first customer match CSV" do
+      result = Grocery::Customer.all
+      expected_first_customer = result[0]
+      expected_first_customer.id.must_equal
+      expected_first_customer.email_address.must_equal
+    end
+    it "The ID, email address of the last customer match CSV" do
+      result = Grocery::Customer.all
+      expected_last_customer = result[-1]
+      expected_last_customer.id.must_equal
+      expected_last_customer.email_address.must_equal
 
-  describe "Customer.find" do
+    end
+  end # => end customers.all
+
+  xdescribe "Customer.find" do
     it "Can find the first customer from the CSV" do
-      # TODO: Your test code here!
+      result = Grocery::Customer.find(1)
+      all_customers = Grocery::Customer.all
+      expected_customer = all_customers[0]
+      result.must_equal expected_customer
     end
 
     it "Can find the last customer from the CSV" do
-      # TODO: Your test code here!
+      result = Grocery::Customer.find(35)
+      all_customers = Grocer::Customers.all
+      expected_customer = all_customers[-1]
+      result.must_equal expected_customer
     end
 
     it "Raises an error for a customer that doesn't exist" do
-      # TODO: Your test code here!
+      proc {Grocery::Customer.find(10000)}.must_raise ArgumentError
     end
   end
 end

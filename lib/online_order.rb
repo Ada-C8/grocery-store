aaf4a2c50a26511c6ed90f8be8d3b2e8c1cd03d7
Shaunna <skwiens@gmail.com>
2017-08-24 14:35:22 -0700

    self.all assembles an array of online_orders. Still need to create customers based on customer ids

---
+++ lib/online_order.rb
@@ -0,0 +1,60 @@
+require_relative 'order.rb'
+require_relative 'customer.rb'
+require 'awesome_print'
+
+module Grocery
+  class OnlineOrder < Grocery::Order
+
+    attr_reader :id, :customer, :status, :products
+
+    def initialize(id, products, customer_id, status)
+      super(id, products)
+      @customer = get_customer(customer_id)
+      @status = status
+    end
+
+    def self.all(csv_file)
+      online_orders = Array.new
+      CSV.read(csv_file).each do |purchase_array|
+        order_id = purchase_array[0].to_i
+        customer_id = purchase_array[2].to_i
+        status = purchase_array[3]
+        products_list = purchase_array[1].split(";")
+        products = {}
+        products_list.each do |product|
+          product_with_price = product.split(":")
+          products[product_with_price[0]] = product_with_price[1].to_f
+        end
+        online_orders << Grocery::OnlineOrder.new(order_id, products,customer_id, status)
+      end
+      return online_orders
+    end
+
+    def self.find(id, csv_file)
+
+    end
+
+    def self.find_by_customer(customer_id)
+
+    end
+
+    def get_customer(customer_id)
+      return Grocery::Customer.find(customer_id, './support/customers.csv')
+    end
+
+    def total
+
+    end
+
+    def add_product
+
+    end
+
+  end
+end
+
+# orders = Grocery::OnlineOrder.all('./support/online_orders.csv')
+
+# online_order = Grocery::OnlineOrder.new(10, {"Jerusalem Artichoke" => 59.92}, 26, "complete")
+#
+# p online_order.customer.class

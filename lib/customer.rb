118b38e7f3f07a81f172b26c5ec1213b7a0a08a4
Eva Sawyer <esawyer25@gmail.com>
2017-08-24 15:14:58 -0700

    made a new file for the customer class

---
+++ lib/customer.rb
@@ -0,0 +1,13 @@
+class Customer
+
+  attr_reader :customer_id, :email, :address
+
+  def initialize(customer_id, email, address)
+    @customer_id = customer_id
+    @email = email
+    @address = address
+  end
+
+  
+
+end #end of customer class

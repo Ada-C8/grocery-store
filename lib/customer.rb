fe66eebd420c70d14f7efa03f2b22c9965d2d3c4
Lauren <Laurenelee88@gmail.com>
2017-08-24 16:49:56 -0700

    added csv read

--- lib/customer.rb
+++ lib/customer.rb
@@ -2,8 +2,19 @@ require 'csv'
 
 module Grocery
   class Customer
+    attr_reader :id, :email, :address
+
+
+    def initialize(id, email, address)
+      @id = id
+      @email = email
+      @address = address
+    end # initialize
+
+# 1,leonard.rogahn@hagenes.org,71596 Eden Route,Connellymouth,LA,98872-9105
 
     def self.all
+      CSV.open("../support/customers.csv", 'r').each
     end # self.all
 
     def self.find(id)

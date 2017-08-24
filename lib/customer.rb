640f28271af3f850aaca9ecd6a22ff5635afe0e3
Lauren <Laurenelee88@gmail.com>
2017-08-24 15:34:55 -0700

    created skeleton of customer.rb file

---
+++ lib/customer.rb
@@ -0,0 +1,12 @@
+require 'csv'
+
+module Grocery
+  class Customer
+
+    def self.all
+    end # self.all
+
+    def self.find(id)
+    end # self.find
+  end # Customer class
+end # Grocery module

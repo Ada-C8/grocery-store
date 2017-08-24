82e9bdbe22d09c21621d8c999eb7093c90af6637
Bennett Rahn <rahnibennett@gmail.com>
2017-08-24 14:42:46 -0700

    add csv logic to input names, change address argument to a hash

--- lib/customer.rb
+++ lib/customer.rb
@@ -10,6 +10,18 @@ module Grocery
 
     def self.all
       customers = []
+      CSV.open("support/customers.csv", 'r').each do |line|
+        id = line[0].to_i
+        email = line[1]
+        address= {
+          address1: line[2],
+          city: line[3],
+          state: line[4],
+          zipcode: line[5]
+        }
+        customers << self.new(id, email, address)
+
+      end
       return customers
     end
   end

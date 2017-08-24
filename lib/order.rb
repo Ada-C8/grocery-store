11cda00cb57c49c8daa836678e376bacab44f6d9
nkiru onwuneme <generalhrh@gmail.com>
2017-08-23 21:49:36 -0700

    Added new order all method

--- lib/order.rb
+++ lib/order.rb
@@ -14,7 +14,7 @@ module Grocery
       products.each do |item, cost|
         sum = (sum + cost)
       end
-      total = ((sum * 0.075) + sum).round(2)
+      return ((sum * 0.075) + sum).round(2)
     end
 
     def add_product(product_name, product_price)
@@ -29,18 +29,24 @@ module Grocery
     def self.all
       orders = []
       products = {}
-      CSV.open("support/orders.csv", "r").each do |line|
-        orders << Grocery::Order.new(line[0], products)
-        line[1].split(";").each do |element|
-          product_list= element.split(":").each do |item, cost|
-            products << product_list
-          end
+
+      CSV.read("./support/orders.csv", "r").each do |line|
+        id = line[0]
+
+        line[1].split(";").each do |product_info|
+          data = product_info.split(":")
+          products[data[0]] = data[1]
         end
+
+        orders << Grocery::Order.new(id, products)
       end
+
+      return orders
     end
 
   end # end of class
 end # module
 
-puts test = Grocery::Order.new(1222, "tomato")
-puts test.csv_file
+Grocery::Order.all.each do |order|
+  puts order.id
+end

59b461fe46433133fe71b16bfc71e45c8249c386
Laura Robertson <mslaura.mae@gmail.com>
2017-08-25 14:47:20 -0700

    Online orders, all using super

--- lib/online_orders.rb
+++ lib/online_orders.rb
@@ -1,20 +1,31 @@
 require 'csv'
-require_relative 'order.rb'
-require_relative 'customers.rb'
+require_relative 'order'
+require_relative 'customers'
 
 module Grocery
   class OnlineOrder < Order
+    @@csv = "../support/online_orders.csv"
+
+    # def initialize
+    #   @csv = "../support/online_orders.csv"
+    # end
 
     def self.all
       super
     end
 
-    # self.all modify order.rb if statment
-    # if self == Grocery::Order
-    # variable = this csv
-    # elsif sellf == Grocery::OnlineOrder
-    # variable = this csv
+    # Maria - split .all method into 3 parts, all has the csv
+    # My proccess could be broken into self.all with csv and the
+    # instance creator
+    # put an if statement in the all method to run extra code
+    # depending on csv provided
   end
 end
 
-puts Grocery::OnlineOrder.all
+# id = 1337
+# products = { "banana" => 1.99, "cracker" => 3.00, "sandwich" => 4.25 }
+# online_order = Grocery::Order.new(id,products)
+# puts online_order.class
+
+p "OnlineOrder"
+p Grocery::OnlineOrder.find(3)

617b4e2124cbbaa7eb3bfefa3cea7f1ec8f9bbe0
Lauren <Laurenelee88@gmail.com>
2017-08-24 18:26:32 -0700

    .all method works with self now

--- lib/customer.rb
+++ lib/customer.rb
@@ -14,14 +14,14 @@ module Grocery
     # 1,leonard.rogahn@hagenes.org,71596 Eden Route,Connellymouth,LA,98872-9105
 
     def self.all
-
+      customers = []
       CSV.open("../support/customers.csv", 'r').each do |line|
         id = line[0].to_i
         email = line[1]
         address = {address: line[2], city: line[3], state: line[4], zipcode: line[5]}
-        customers = [id, email, address]
-        return customers
+        customers << self.new(id, email, address)
       end
+      return customers
     end # self.all
 
     def self.find(id)

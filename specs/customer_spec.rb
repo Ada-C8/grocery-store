a55c38cddd6d194b76730035576f56b02b3ab514
Sarah Read-Brown <srbusiness2@gmail.com>
2017-08-24 20:56:42 -0700

    Adds test 1 for customers.find method

--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -7,18 +7,22 @@ require_relative '../lib/customer'
 # require_relative '../lib/customer'
 # 1,leonard.rogahn@hagenes.org,71596 Eden Route,Connellymouth,LA,98872-9105
 describe "Customer" do
+  before do
+    @customer_first = Grocery::Customer.new(1, "leonard.rogahn@hagenes.org", {address1: "71596 Eden Route" , city: "Connellymouth" , state: "LA", zip_code: "98872-9105" })
+    @customer_last = Grocery::Customer.new(35, "rogers_koelpin@oconnell.org", {address1: "7513 Kaylee Summit" , city: "Uptonhaven" , state: "DE", zip_code: "64529-2614" })
+  end
   describe "#initialize" do
     #"leonard.rogahn@hagenes.org", "71596 Eden Route,Connellymouth,LA,98872-9105")
     # {address1: , city: , state: , zipcode: }
     it "Takes an ID, email and address info" do
-      customer = Grocery::Customer.new(1, "leonard.rogahn@hagenes.org", {address1: "71596 Eden Route" , city: "Connellymouth" , state: "LA", zip_code: "98872-9105" })
+      # customer = Grocery::Customer.new(1, "leonard.rogahn@hagenes.org", {address1: "71596 Eden Route" , city: "Connellymouth" , state: "LA", zip_code: "98872-9105" })
 
       address = {:address1=>"71596 Eden Route", :city=>"Connellymouth",:state=>"LA", :zip_code=>"98872-9105"}
 
-      customer.must_respond_to :id && :email && :address
-      customer.id.must_equal 1
-      customer.email.must_equal "leonard.rogahn@hagenes.org"
-      customer.address.must_equal address
+      @customer_first.must_respond_to :id && :email && :address
+      @customer_first.id.must_equal 1
+      @customer_first.email.must_equal "leonard.rogahn@hagenes.org"
+      @customer_first.address.must_equal address
     end
   end
 
@@ -47,22 +51,20 @@ describe "Customer" do
 
     #this could be more efficient test
     it "the ID email and address customers.all[0] and customers.all[35] match what is in the CSV file" do
-      customer_one = Grocery::Customer.new(1, "leonard.rogahn@hagenes.org", {address1: "71596 Eden Route" , city: "Connellymouth" , state: "LA", zip_code: "98872-9105" })
-      # binding.pry
-      customer = Grocery::Customer.all
-      customer.first.id.must_equal customer_one.id
-
-      Grocery::Customer.all.first.email.must_equal customer_one.email
-      #
-      Grocery::Customer.all.first.address.must_equal customer_one.address
-
-      Grocery::Customer.all.first.email.must_equal customer_one.email
+      Grocery::Customer.all.first.id.must_equal @customer_first.id
+      Grocery::Customer.all.first.address.must_equal @customer_first.address
+      Grocery::Customer.all.first.email.must_equal @customer_first.email
+      Grocery::Customer.all.last.id.must_equal @customer_last.id
+      Grocery::Customer.all.last.address.must_equal @customer_last.address
+      Grocery::Customer.all.last.email.must_equal @customer_last.email
     end
   end
 
-  xdescribe "Customer.find" do
+  describe "Customer.find" do
     it "Can find the first customer from the CSV" do
-      # TODO: Your test code here!
+      Grocery::Customer.find(1).id.must_equal @customer_first.id
+      Grocery::Customer.find(1).email.must_equal @customer_first.email      Grocery::Customer.find(1).address.must_equal @customer_first.address
+
     end
 
     it "Can find the last customer from the CSV" do

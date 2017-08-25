fda8c7d70b5c1aba6ebb3d7ce68c2eb2d3dd22ca
Lauren <Laurenelee88@gmail.com>
2017-08-25 15:18:41 -0700

    total method written

--- lib/online_order.rb
+++ lib/online_order.rb
@@ -1,25 +1,33 @@
 require 'csv'
-
+require_relative 'order'
 
 module Grocery
   class OnlineOrder < Order
-
+    attr_reader :customer_id, :status
     def initialize(id, products, customer_id, status)
-      super
+      super(id, products)
       @customer_id = customer_id
       @status = status.to_sym
     end #initialize
 
+    # def customer(customer_id)
+    #   # Customer.find
+    # end
+
     def total
-      super
-    end #total
+      if super == 0
+        return 0
+      else
+        super + 10
+      end
+    end
 
     def add_product
       super
     end #add_product
 
     def self.all
-      # not using super b/c new csv and taking in more arguments (we don't want anything from Order.all from Order file) we want ALL of the online_orders 
+      # not using super b/c new csv and taking in more arguments (we don't want anything from Order.all from Order file) we want ALL of the online_orders
     end #self.all
 
     def self.find(id)

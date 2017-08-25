a2607dd71b4106168c2c13c849ea7ab9cd20c824
Sara Frandsen <sara.frandsen@gmail.com>
2017-08-25 14:24:04 -0700

    complete wave 1 tests and code, creating initialize, total, and add methods.

--- lib/order.rb
+++ lib/order.rb
@@ -3,16 +3,24 @@ module Grocery
     attr_reader :id, :products
 
     def initialize(id, products)
-      @id = id
+      @id = id.to_i
       @products = products
     end
 
     def total
       # TODO: implement total
+      sum = products.values.inject(0, :+)
+      return sum + (sum * 0.075).round(2)
     end
 
     def add_product(product_name, product_price)
       # TODO: implement add_product
+      unless @products.has_key?(product_name) # as long as the collection of products doesn't already have the key of the new item, you can add it to the list
+        @products[product_name] = product_price
+        return true
+      else
+        return false # otherwise, it isn't unique, and something with that name is already on file.
+      end
     end
   end
 end

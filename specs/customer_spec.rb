c261be14fc4ffc22d023c593cb68b1ab00fedf75
Sarah Read-Brown <srbusiness2@gmail.com>
2017-08-24 17:40:30 -0700

    Add tests for customer. all method

--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -22,20 +22,44 @@ describe "Customer" do
     end
   end
 
+  # TODO: Your test code here!
+  # Useful checks might include:
+  #   - Customer.all returns an array
+  #   - Everything in the array is a Customer
+  #   - The number of orders is correct
+  #   - The ID, email address of the first and last
+  #       customer match what's in the CSV file
+  # Feel free to split this into multiple tests if needed
   describe "Customer.all" do
-    it "Returns an array of all customers" do
-      # TODO: Your test code here!
-      # Useful checks might include:
-      #   - Customer.all returns an array
-      #   - Everything in the array is a Customer
-      #   - The number of orders is correct
-      #   - The ID, email address of the first and last
-      #       customer match what's in the CSV file
-      # Feel free to split this into multiple tests if needed
+    it "Customer.all returns an array" do
+      Grocery::Customer.all.must_be_kind_of Array
+    end
+
+    it "Everything in the array is of class customer" do
+      Grocery::Customer.all.each do |customer|
+        customer.must_be_kind_of Grocery::Customer
+      end
+    end
+
+    it "Customer.all returns and array with the correct number of customers" do
+      Grocery::Customer.all.length.must_equal 35
+    end
+
+    #this could be more efficient test
+    it "the ID email and address customers.all[0] and customers.all[35] match what is in the CSV file" do
+      customer_one = Grocery::Customer.new(1, "leonard.rogahn@hagenes.org", {address1: "71596 Eden Route" , city: "Connellymouth" , state: "LA", zip_code: "98872-9105" })
+      # binding.pry
+      customer = Grocery::Customer.all
+      customer.first.id.must_equal customer_one.id
+
+      Grocery::Customer.all.first.email.must_equal customer_one.email
+
+      Grocery::Customer.all.first.email.must_equal customer_one.address
+
     end
   end
 
-  describe "Customer.find" do
+  xdescribe "Customer.find" do
     it "Can find the first customer from the CSV" do
       # TODO: Your test code here!
     end

7aa3d29cc9c97063dc00eda6219f20c88adc7473
enigmagnetic <lauren.cardella@gmail.com>
2017-08-25 00:08:32 -0700

    Build and pass OnlineOrder.all. Add headers to customer and online order CSVs.

--- lib/online_order.rb
+++ lib/online_order.rb
@@ -1,17 +1,56 @@
 require_relative './order.rb'
 
-class OnlineOrder < Grocery::Order
-  attr_reader :customer
-  attr_accessor :status
+module Grocery
+  class OnlineOrder < Grocery::Order
+    attr_reader :customer
+    attr_accessor :status
 
-  def initialize(order_id, customer_id, order_status = :pending, products = nil)
-    @id = order_id
-    @customer = customer_id
-    @products = products
-    @status = order_status
-  end
+    def initialize(order_id, customer_id, order_status = :pending, products = nil)
+      @id = order_id
+      @customer = customer_id
+      @products = products
+      @status = order_status
+    end
 
-  def total
-    return @products == nil ? 0 : super + 10
+    def self.all
+      orders = []
+      CSV.open('./support/online_orders.csv', "r", headers: true).each do |row|
+        order_products = row["products"].split(";")
+        products = {}
+        order_products.each do |product_price|
+          product_hash = product_price.split(":")
+          products.store(product_hash[0], product_hash[1].to_f)
+        end
+        orders << OnlineOrder.new(row["id"].to_i, row["customer_id"], row["status"].to_sym, products)
+      end
+      orders
+    end
+
+    def total
+      return @products == nil ? 0 : super + 10
+    end
+
+    def print
+      puts 'Hi'
+    end
+
+    def add_product(product_name, product_price)
+      if @status == :pending || @status == :paid
+        super
+      else
+       return "This order cannot be modified."
+      end
+    end
   end
 end
+
+
+# begin
+#   if @status == :pending || @status == :paid
+#     super
+#   else
+#    raise ArgumentError.new("This order cannot be modified.")
+#   end
+# rescue => e
+#   puts e.message
+# end
--- specs/online_order_spec.rb
+++ specs/online_order_spec.rb
@@ -15,24 +15,24 @@ describe "OnlineOrder" do
   describe "#initialize" do
     it "Is a kind of Order" do
       # Check that an OnlineOrder is in fact a kind of Order
-      online_order = OnlineOrder.new(1337, 3)
+      online_order = Grocery::OnlineOrder.new(1337, 3)
       online_order.must_be_kind_of Grocery::Order
     end
 
     it "Can access Customer object" do
-      online_order = OnlineOrder.new(1337, 3, :paid)
+      online_order = Grocery::OnlineOrder.new(1337, 3, :paid)
       online_order.must_respond_to :customer
       online_order.customer.must_equal 3
     end
 
     it "Can access the online order status" do
-      online_order = OnlineOrder.new(1337, 3, :paid)
+      online_order = Grocery::OnlineOrder.new(1337, 3, :paid)
       online_order.must_respond_to :status
       online_order.status.must_equal :paid
     end
 
     it "Has default order status of :pending" do
-      online_order = OnlineOrder.new(1337, 3)
+      online_order = Grocery::OnlineOrder.new(1337, 3)
       online_order.status.must_equal :pending
     end
   end
@@ -40,36 +40,55 @@ describe "OnlineOrder" do
   describe "#total" do
     it "Adds a shipping fee" do
       reg_order = Grocery::Order.new(1337, {"banana" => 1.0, "apple" => 1.25, "whole milk" => 3.59})
-      online_order = OnlineOrder.new(1337, 3, :pending, {"banana" => 1.0, "apple" => 1.25, "whole milk" => 3.59})
+      online_order = Grocery::OnlineOrder.new(1337, 3, :pending, {"banana" => 1.0, "apple" => 1.25, "whole milk" => 3.59})
       online_order.total.must_equal (reg_order.total + 10)
     end
 
     it "Doesn't add a shipping fee if there are no products" do
-      online_order = OnlineOrder.new(1337, 3)
+      online_order = Grocery::OnlineOrder.new(1337, 3)
       online_order.total.must_equal 0
     end
   end
 
-  xdescribe "#add_product" do
+  describe "#add_product" do
     it "Does not permit action for processing, shipped or completed statuses" do
-      # TODO: Your test code here!
+      online_order = Grocery::OnlineOrder.new(1337, 3, :shipped, {"banana" => 1.0, "apple" => 1.25, "whole milk" => 3.59})
+      online_order.add_product("olives", 4.59)
+      online_order.products.has_key?("olives").must_equal false # TODO: Your test code here!
+    end
+
+    xit "Raise ArgumentError for processing, shipped, or completed statuses" do
+      online_order = Grocery::OnlineOrder.new(1337, 3, :procesing, {"banana" => 1.0, "apple" => 1.25, "whole milk" => 3.59})
+      online_order.add_product("olives", 4.59).must_raise ArgumentError
     end
 
     it "Permits action for pending and paid satuses" do
-      # TODO: Your test code here!
+      online_order = Grocery::OnlineOrder.new(1337, 3, :paid, {"banana" => 1.0, "apple" => 1.25, "whole milk" => 3.59})
+      online_order.add_product("olives", 4.59)
+      online_order.products.has_key?("olives").must_equal true# TODO: Your test code here!
     end
   end
 
-  xdescribe "OnlineOrder.all" do
+  describe "OnlineOrder.all" do
     it "Returns an array of all online orders" do
-      # TODO: Your test code here!
-      # Useful checks might include:
-      #   - OnlineOrder.all returns an array
-      #   - Everything in the array is an Order
-      #   - The number of orders is correct
-      #   - The customer is present
-      #   - The status is present
-      # Feel free to split this into multiple tests if needed
+      Grocery::OnlineOrder.all.must_be_instance_of Array
+      Grocery::OnlineOrder.all.each { |order| order.must_be_instance_of Grocery::OnlineOrder }
+    end
+    # TODO: Your test code here!
+    # Useful checks might include:
+    #   - OnlineOrder.all returns an array
+    #   - Everything in the array is an Order
+    #   - The number of orders is correct
+    #   - The customer is present
+    #   - The status is present
+    # Feel free to split this into multiple tests if needed
+    it "Has the correct number of orders" do
+      Grocery::Order.all.length.must_equal 100
+    end
+
+    it "Has a customer and status for every order" do
+      Grocery::OnlineOrder.all.each { |order| order.customer.wont_be_nil }
+      Grocery::OnlineOrder.all.each { |order| order.status.must_be_instance_of Symbol }
     end
   end
 

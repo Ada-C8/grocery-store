84d1929513bd86051814695a3950e545a04756f3
Bennett Rahn <rahnibennett@gmail.com>
2017-08-25 12:29:19 -0700

    cange to use class var

--- lib/customer.rb
+++ lib/customer.rb
@@ -4,6 +4,8 @@ module Grocery
   class Customer
     attr_reader :id, :email, :address
 
+    @@customers = []
+
     def initialize(id, email, address)
       @id = id
       @email = email
@@ -11,7 +13,9 @@ module Grocery
     end
 
     def self.all
-      customers = []
+      if @@customers.length > 0
+        return @@customers
+      end
       CSV.open("support/customers.csv", 'r').each do |line|
         id = line[0].to_i
         email = line[1]
@@ -21,21 +25,21 @@ module Grocery
           state: line[4],
           zipcode: line[5]
         }
-        customers << self.new(id, email, address)
+        @@customers << self.new(id, email, address)
 
       end
-      return customers
+      return @@customers
     end
 
     def self.find(id)
-      customers = self.all
+      # customers = self.all
       # logic error:
-      until (1..customers.length).include?(id) #&& (id.kind_of? Integer)
+      until (1..@@customers.length).include?(id) #&& (id.kind_of? Integer)
         raise ArgumentError.new("Invalid id: #{id}")
       end
 
       found_customer = nil
-      customers.each do |customer|
+      @@customers.each do |customer|
         if customer.id == id
           found_customer = customer
         end

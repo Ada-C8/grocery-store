0f719b7a9fa6734f9cc97806fc8bce85421d1a68
Severin Wiggenhorn <severin.randall@gmail.com>
2017-08-24 19:38:18 -0700

    added tests for .all and .find class methods

--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -3,12 +3,35 @@ require 'minitest/reporters'
 require 'minitest/skip_dsl'
 
 # TODO: uncomment the next line once you start wave 3
-# require_relative '../lib/customer'
+require_relative '../lib/customer'
 
-xdescribe "Customer" do
+describe "Customer" do
   describe "#initialize" do
     it "Takes an ID, email and address info" do
-      # TODO: Your test code here!
+
+      id = 112
+      email = "bob@aol.com"
+      address_1 = "1111 Main Street"
+      city = "Smalltown"
+      state = "Anywhere"
+      zip = "99999"
+
+      customer = Grocery::Customer.new(id, email, address_1, city, state, zip)
+
+      customer.must_respond_to :id
+      customer.must_respond_to :email
+      customer.must_respond_to :address_1
+      customer.must_respond_to :city
+      customer.must_respond_to :state
+      customer.must_respond_to :zip
+
+      customer.id.must_equal 112
+      customer.email.must_equal "bob@aol.com"
+      customer.address_1.must_equal "1111 Main Street"
+      customer.city.must_equal "Smalltown"
+      customer.state.must_equal "Anywhere"
+      customer.zip.must_equal "99999"
+
     end
   end
 
@@ -22,20 +45,26 @@ xdescribe "Customer" do
       #   - The ID, email address of the first and last
       #       customer match what's in the CSV file
       # Feel free to split this into multiple tests if needed
+      all_the_customers = Grocery::Customer.all("./support/customers.csv")
+      all_the_customers.must_be_instance_of Array
+      all_the_customers.length.must_equal 35
+
+      all_the_customers[0][:id].must_equal 1
+      all_the_customers[34][:id].must_equal 35
+      all_the_customers[0][:email].must_equal "leonard.rogahn@hagenes.org"
+      all_the_customers[34][:email].must_equal "rogers_koelpin@oconnell.org"
     end
   end
 
   describe "Customer.find" do
     it "Can find the first customer from the CSV" do
-      # TODO: Your test code here!
+      Grocery::Customer.find("./support/customers.csv", 1)
     end
 
     it "Can find the last customer from the CSV" do
-      # TODO: Your test code here!
-    end
+      Grocery::Customer.find("./support/customers.csv", 35)    end
 
-    it "Raises an error for a customer that doesn't exist" do
-      # TODO: Your test code here!
+      it "Raises an error for a customer that doesn't exist" do
+        proc {Grocery::Customer.find("./support/customers.csv", 45)}.must_raise ArgumentError    end
+      end
     end
-  end
-end

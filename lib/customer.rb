ce3a9c00b6c7140da93ed305dc39ea42d193c4db
Severin Wiggenhorn <severin.randall@gmail.com>
2017-08-24 22:47:05 -0700

    updated self.all method to ensure @all_customers contains instances of customer

--- lib/customer.rb
+++ lib/customer.rb
@@ -5,53 +5,55 @@ require 'awesome_print'
 
 module Grocery
 
-class Customer
+  class Customer
 
-  attr_reader :id, :email, :address_1, :city, :state, :zip, :all_customers
+    attr_reader :id, :email, :address_1, :city, :state, :zip, :all_customers
 
-  def initialize(id, email, address_1, city, state, zip)
-    @id = id
-    @email = email
-    @address_1 = address_1
-    @city = city
-    @state = state
-    @zip = zip
-  end
-
-
-  def self.all(file_name)
-    @all_customers = []
-    # each_customer = {}
-    CSV.open(file_name, "r").each do |row|
-      @id = row[0].to_i
-      @email = row[1]
-      @address_1 = row[2]
-      @city = row[3]
-      @state = row[4]
-      @zip = row[5]
-      @each_customer = { id: @id, email: @email, address_1: @address_1, city: @city, zip: @zip}
-      # @each_customer = row.split(",")
-      @all_customers << @each_customer
+    def initialize(id, email, address_1, city, state, zip)
+      @id = id
+      @email = email
+      @address_1 = address_1
+      @city = city
+      @state = state
+      @zip = zip
     end
-    return @all_customers
-  end
 
-  def self.find(file_name, id)
-    self.all(file_name).each do |instance|
-      if instance[:id] == id
-        return instance
+
+    def self.all(file_name)
+      @all_customers = []
+      CSV.open(file_name, "r").each do |row|
+        @id = row[0].to_i
+        @email = row[1]
+        @address_1 = row[2]
+        @city = row[3]
+        @state = row[4]
+        @zip = row[5]
+        # @each_customer = { id: @id, email: @email, address_1: @address_1, city: @city, zip: @zip}
+        @all_customers << Grocery::Customer.new(@id, @email, @address_1, @city, @state, @zip)
       end
+      return @all_customers
     end
+
+    def self.find(file_name, id)
+      self.all(file_name).each do |instance|
+        if instance.id == id
+          return instance
+        end
+      end
       raise ArgumentError.new "Invalid Customer Number"
+    end
+
+
   end
 
-
 end
 
-end
+all_the_customers = Grocery::Customer.all("./support/customers.csv")
 
-# all_the_customers = Grocery::Customer.all("./support/customers.csv")
+# ap all_the_customers
 
+#
+ap all_the_customers
 customer = Grocery::Customer.find("./support/customers.csv", 1)
 
-puts customer
+puts customer.class

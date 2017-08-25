f8994795aad4bbcdec10d404c047d54469894360
Kimberley Mackenzie <kimberleyamackenzie@gmail.com>
2017-08-25 13:09:16 -0700

    Fixed redundancy in find by customer method tests.

--- specs/online_order_spec.rb
+++ specs/online_order_spec.rb
@@ -17,15 +17,16 @@ describe "OnlineOrder" do
     end
 
     it "Can access the online order status" do
-    online_order = Grocery::OnlineOrder.new(53, {"banana" => 1.99, "cracker" => 3.00}, 24, status: :paid)
-    online_order.status.must_be_instance_of Symbol
-    online_order.status.must_equal :paid
+      online_order = Grocery::OnlineOrder.new(53, {"banana" => 1.99, "cracker" => 3.00}, 24, status: :paid)
+      online_order.status.must_be_instance_of Symbol
+      online_order.status.must_equal :paid
     end
   end
 
   describe "#total" do
     it "Adds a shipping fee" do
       online_order = Grocery::OnlineOrder.new(53, {"banana" => 1.99, "cracker" => 3.00}, 24, status: :paid)
+
       test_total = (4.99 + (4.99 * 0.075).round(2)) + 10
       online_order.total.must_equal test_total
     end
@@ -38,17 +39,15 @@ describe "OnlineOrder" do
 
   describe "#add_product" do
     it "Does not permit action for processing, shipped or completed statuses" do
+
       online_order = Grocery::OnlineOrder.new(53, {"banana" => 1.99, "cracker" => 3.00}, 24, status: :processing)
-      online_order.add_product("salmon", 8.99)
-      proc {online_order.add_product("salmon", 8.99)  }.must_raise ArgumentError
+      proc {online_order.add_product("salmon", 8.99)}.must_raise ArgumentError
 
       online_order = Grocery::OnlineOrder.new(53, {"banana" => 1.99, "cracker" => 3.00}, 24, status: :shipped)
-      online_order.add_product("salmon", 8.99)
-      proc {online_order.add_product("salmon", 8.99)  }.must_raise ArgumentError
+      proc {online_order.add_product("salmon", 8.99)}.must_raise ArgumentError
 
       online_order = Grocery::OnlineOrder.new(53, {"banana" => 1.99, "cracker" => 3.00}, 24, status: :completed)
-      online_order.add_product("salmon", 8.99)
-      proc {online_order.add_product("salmon", 8.99)  }.must_raise ArgumentError
+      proc {online_order.add_product("salmon", 8.99)}.must_raise ArgumentError
     end
 
     it "Permits action for pending and paid satuses" do
@@ -64,20 +63,50 @@ describe "OnlineOrder" do
 
   describe "OnlineOrder.all" do
     it "Returns an array of all online orders" do
-      # TODO: Your test code here!
-      # Useful checks might include:
-      #   - OnlineOrder.all returns an array
-      #   - Everything in the array is an Order
-      #   - The number of orders is correct
-      #   - The customer is present
-      #   - The status is present
-      # Feel free to split this into multiple tests if needed
+      Grocery::OnlineOrder.all.must_be_instance_of Array
+      Grocery::OnlineOrder.all[17].must_be_instance_of Grocery::OnlineOrder
+    end
+
+    it "each object in the array is an order" do
+      order = Grocery::OnlineOrder.all[1]
+      order.must_be_instance_of Grocery::OnlineOrder
+
+      order = Grocery::OnlineOrder.all[99]
+      order.must_be_instance_of Grocery::OnlineOrder
+
+      order = Grocery::OnlineOrder.all[12]
+      order.must_be_instance_of Grocery::OnlineOrder
+    end
+
+    it "has correct order status" do
+      order = Grocery::OnlineOrder.all[0]
+      order.status.must_equal :complete
+
+      order = Grocery::OnlineOrder.all[99]
+      order.status.must_equal :pending
+
+      Grocery::OnlineOrder.all[18].status.must_be_instance_of Symbol
+    end
+
+    it "The number of online orders is correct" do
+      Grocery::OnlineOrder.all.count.must_equal 100
     end
   end
+end
 
-  describe "OnlineOrder.find_by_customer" do
-    it "Returns an array of online orders for a specific customer ID" do
-      # TODO: Your test code here!
-    end
+describe "OnlineOrder.find_by_customer" do
+  it "Returns an array of online orders for a specific customer ID" do
+    Grocery::OnlineOrder.find_by_customer(26).must_be_instance_of Array
+
+    orders = Grocery::OnlineOrder.find_by_customer(26)
+    orders[0].must_be_instance_of Grocery::OnlineOrder
+  end
+
+  it "Raises an error for a customer that doesn't exist" do
+    proc {Grocery::OnlineOrder.find_by_customer("1000")}.must_raise ArgumentError
+
+    proc {Grocery::OnlineOrder.find_by_customer("-3")}.must_raise ArgumentError
+
+    proc {Grocery::OnlineOrder.find_by_customer("0")}.must_raise ArgumentError
   end
 end

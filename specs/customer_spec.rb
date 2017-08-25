9a86f6dd5dbab3684ff28fabb1fb93c66de1c338
Julia <julia.meier@gmail.com>
2017-08-25 13:19:39 -0700

    Customer class passes Customer.all tests

--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -35,17 +35,26 @@ describe "Customer" do
 
     it "Everything in the array is a Customer" do
       customer = Grocery::Customer.all
-      customer[0].must_be_instance_of Customer
-
+      customer[0].must_be_instance_of Grocery::Customer
     end
 
     it "returns the correct number of orders" do
+      customer = Grocery::Customer.all
+      customer.length.must_equal 35
     end
 
     it "Returns the ID, email and address of the first customer (in the CSV)" do
+      customer = Grocery::Customer.all
+      customer[0].id.must_equal "1"
+      customer[0].email.must_equal "leonard.rogahn@hagenes.org"
+      customer[0].address.must_equal "71596 Eden Route, Connellymouth, LA, 98872-9105"
     end
 
     it "Returns the ID, email and address of the last customer" do
+      customer = Grocery::Customer.all
+      customer[34].id.must_equal "35"
+      customer[34].email.must_equal "rogers_koelpin@oconnell.org"
+      customer[34].address.must_equal "7513 Kaylee Summit, Uptonhaven, DE, 64529-2614"
     end
   end
 

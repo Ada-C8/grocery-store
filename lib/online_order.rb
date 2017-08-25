98185660c84244f33c2d36dbcf0c500c2e3c0af6
Lauren <Laurenelee88@gmail.com>
2017-08-25 12:33:18 -0700

    prep methods and super calls

--- lib/online_order.rb
+++ lib/online_order.rb
@@ -2,9 +2,29 @@ require 'csv'
 
 
 module Grocery
-  class OnlineOrder < Order 
+  class OnlineOrder < Order
 
+    def initialize(id, products, customer_id, status)
+      super
+      @customer_id = customer_id
+      @status = status.to_sym
+    end #initialize
 
+    def total
+      super
+    end #total
+
+    def add_product
+      super
+    end #add_product
+
+    def self.all
+      # not using super b/c new csv and taking in more arguments (we don't want anything from Order.all from Order file) we want ALL of the online_orders 
+    end #self.all
+
+    def self.find(id)
+      super
+    end #self.find(id)
 
   end # Customer class
 end # Grocery module

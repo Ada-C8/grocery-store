496c830af1178d676d7d1c1967f9663ea3660c94
Kimberley Mackenzie <kimberleyamackenzie@gmail.com>
2017-08-25 13:51:33 -0700

    Broadened order.find tests, made all IDs integers.

--- specs/order_spec.rb
+++ specs/order_spec.rb
@@ -4,12 +4,13 @@ require 'minitest/skip_dsl'
 require 'csv'
 require_relative '../lib/order'
 
-
 describe "Order Wave 1" do
   describe "#initialize" do
     it "Takes an ID and collection of products" do
       id = 1337
       order = Grocery::Order.new(id, {})
+
+      order.must_be_instance_of Grocery::Order
 
       order.must_respond_to :id
       order.id.must_equal id
@@ -29,6 +30,8 @@ describe "Order Wave 1" do
       expected_total = sum + (sum * 0.075).round(2)
 
       order.total.must_equal expected_total
+
+      order.total.must_be_instance_of Float
     end
 
     it "Returns a total of zero if there are no products" do
@@ -61,33 +64,27 @@ describe "Order Wave 1" do
       products = { "banana" => 1.99, "cracker" => 3.00 }
 
       order = Grocery::Order.new(1337, products)
-      before_total = order.total
-
-      result = order.add_product("banana", 4.25)
-      after_total = order.total
-
-      result.must_equal false
-      before_total.must_equal after_total
+      order.add_product("banana", 4.25).must_equal false
     end
 
     it "Returns true if the product is new" do
       products = { "banana" => 1.99, "cracker" => 3.00 }
       order = Grocery::Order.new(1337, products)
 
-      result = order.add_product("salad", 4.25)
-      result.must_equal true
+      order.add_product("salad", 4.25).must_equal true
+
+      order.products.count.must_equal 3
     end
   end
 
   describe "#remove_product" do
-
     it "removes product from collection" do
       products = { "banana" => 1.99, "cracker" => 3.00 }
       order = Grocery::Order.new(1337, products)
       order.remove_product("banana")
 
-      result = order.products.keys.include?("banana")
-      result.must_equal false
+      order.products.keys.include?("banana").must_equal false
+      order.products.count.must_equal 1
     end
 
     it "returns true if the item was successfully removed" do
@@ -117,32 +114,33 @@ describe "Order Wave 2" do
     end
 
     it "must contain the correct amount of orders in the all orders array" do
-    Grocery::Order.all.length.must_equal 100
+      Grocery::Order.all.count.must_equal 100
     end
 
     it "The ID and products of the first and last orders match what's in the CSV file" do
-      Grocery::Order.all[0].id.must_equal "1"
+      Grocery::Order.all[0].id.must_equal 1
       Grocery::Order.all[0].products.must_equal [{"Slivered Almonds" => "22.88"}, {"Wholewheat flour" => "1.93"}, {"Grape Seed Oil" => "74.9"}]
 
-      Grocery::Order.all[99].id.must_equal "100"
+      Grocery::Order.all[99].id.must_equal 100
       Grocery::Order.all[99].products.must_equal [{"Allspice" => "64.74"}, {"Bran" => "14.72"}, {"UnbleachedFlour" => "80.59"}]
     end
-
   end
 
   describe "Order.find" do
     it "Can find the first order from the CSV" do
-      Grocery::Order.find("1").must_be_instance_of Grocery::Order
+      Grocery::Order.find(1).must_be_instance_of Grocery::Order
+      Grocery::Order.find(1).id.must_equal 1
     end
 
     it "Can find the last order from the CSV" do
-      Grocery::Order.find("99").must_be_instance_of Grocery::Order
+      Grocery::Order.find(99).must_be_instance_of Grocery::Order
+      Grocery::Order.find(99).id.must_equal 99
     end
 
     it "Raises an error for an order that doesn't exist" do
-      proc {Grocery::Order.find("1000")}.must_raise ArgumentError
-      proc {Grocery::Order.find("0")}.must_raise ArgumentError
-      proc {Grocery::Order.find("-12")}.must_raise ArgumentError
+      proc {Grocery::Order.find(1000)}.must_raise ArgumentError
+      proc {Grocery::Order.find(0)}.must_raise ArgumentError
+      proc {Grocery::Order.find(-12)}.must_raise ArgumentError
     end
   end
 end

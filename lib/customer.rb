75e23eb7bb9c15f9aa10e83043f133db16155638
Eva Sawyer <esawyer25@gmail.com>
2017-08-25 08:18:37 -0700

    fixed self.all to return cutomers

--- lib/customer.rb
+++ lib/customer.rb
@@ -15,19 +15,27 @@ class Customer
     #1: Read the csv file
     #cvs line = id_number, email, housenumber street,town, state, zipcode
     #make into [(id_number, email, (address))]
-    all_customers = []
+    cust_info_arr = []
     CSV.open("support/customers.csv", 'r').each do |line|
       one_customer =[]
       customer_id = line[0]
       email = line[1]
       address = line[2..5]
       one_customer << customer_id << email << address
-      all_customers << one_customer
+      cust_info_arr << one_customer
     end
-    return all_customers
+    customers = []
+
+    cust_info_arr.each do |single_cust_info|
+      customers << Customer.new(single_cust_info[0], single_cust_info[1], single_cust_info[2])
+    end
+    return customers
   end
 end #end of customer class
 
+# all_customers = Customer.all
+# puts all_customers.class
+# puts all_customers[0].class
 # customer_id = 13
 # email = "bert@yahoo.com"
 # address = "12 Caldwell Ln, Nashvegas, TN 43254"

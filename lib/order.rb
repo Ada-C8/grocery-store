21021d26785e4ba485d724c760b7a8f4d2e4cd72
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-08-25 15:21:32 -0700

    streamline #total method

--- lib/order.rb
+++ lib/order.rb
@@ -1,9 +1,19 @@
+# require 'awesome_print'
+# require_relative 'customer'
+# require_relative 'online_order'
+
 module Grocery
+
+  require 'csv'
+
   class Order
-    attr_reader :id, :products
+
+    attr_reader :id, :products, :all_orders
+
+    # @@all_orders = []
 
     def initialize(id, products)
-      @id = id
+      @id = id.to_i
       @products = products
     end
 
@@ -34,6 +44,39 @@ module Grocery
       end
     end
 
+    # def self.all_orders
+    #   return @@all_orders
+    # end
+
+    def self.all(filename)
+      all_orders = []
+
+      CSV.open(filename).each do |order|
+        id = order.shift
+        products = []
+
+        order[0].split(';').each do |item|
+          product = item.split(':')
+          products << {product[0] => product[1].to_f}
+        end
+
+        all_orders << self.new(id, products)
+
+      end
+      return all_orders
+    end
+
+    def self.find(id, orders)
+      orders.each do |order|
+        if order.id == id
+          return order
+        end
+      end
+      # if it didn't find the id:
+      raise RangeError.new("ID does not exist")
+    end
+
   end # end of Order class definition
+
 
 end # end of Grocery module definition
--- specs/order_spec.rb
+++ specs/order_spec.rb
@@ -3,18 +3,23 @@ require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require_relative '../lib/order'
 
+order_list = "./support/orders.csv"
+# online_order_list = "./support/online_orders.csv"
+# test_groceries = Grocery::Order.all
+
 describe "Order Wave 1" do
   describe "#initialize" do
     it "Takes an ID and collection of products" do
       id = 1337
-      order = Grocery::Order.new(id, {})
+      order = Grocery::Order.new(id, {"food" => 10.00})
 
       order.must_respond_to :id
       order.id.must_equal id
       order.id.must_be_kind_of Integer
 
       order.must_respond_to :products
-      order.products.length.must_equal 0
+      order.products.length.must_equal 1
+      order.products.must_be_kind_of Hash
     end
   end
 
@@ -105,7 +110,9 @@ describe "Order Wave 1" do
 end
 
 # TODO: change 'xdescribe' to 'describe' to run these tests
-xdescribe "Order Wave 2" do
+describe "Order Wave 2" do
+  test_groceries = Grocery::Order.all(order_list)
+
   describe "Order.all" do
     it "Returns an array of all orders" do
       # TODO: Your test code here!
@@ -116,20 +123,37 @@ xdescribe "Order Wave 2" do
       #   - The ID and products of the first and last
       #       orders match what's in the CSV file
       # Feel free to split this into multiple tests if needed
+      test_groceries.must_be_kind_of Array
+
+      test_groceries.each do |order|
+        order.must_be_kind_of Grocery::Order
+      end
+
+      test_groceries.length.must_equal CSV.readlines("./support/orders.csv").size
+
+      test_groceries.first.id.must_equal 1
+      test_groceries.last.id.must_equal 100
     end
   end
 
   describe "Order.find" do
+    it "Can return an Order" do
+      Grocery::Order.find(1, test_groceries).must_be_kind_of Grocery::Order
+    end
+
     it "Can find the first order from the CSV" do
-      # TODO: Your test code here!
+      # puts Grocery::Order.all_orders.first
+      Grocery::Order.find(1, test_groceries).must_equal test_groceries.first
     end
 
     it "Can find the last order from the CSV" do
       # TODO: Your test code here!
+      Grocery::Order.find(100, test_groceries).must_equal test_groceries.last
     end
 
     it "Raises an error for an order that doesn't exist" do
       # TODO: Your test code here!
+      proc {Grocery::Order.find(1000000, test_groceries)}.must_raise RangeError
     end
   end
 end

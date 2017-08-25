f7bc25cff660958ac3b5112988934cb0abe5db8a
Kimberley Zell <kimberley.zell@gmail.com>
2017-08-25 14:13:05 -0700

    Completed self.all funtionality and tested. Put under Grocery Module.

--- lib/Customer.rb
+++ lib/Customer.rb
@@ -1,6 +1,9 @@
+require 'csv'
+require 'pry'
+
+module Grocery
 class Customer
   attr_reader :id, :email, :address
-
 
   def initialize(id, email, address)
     @id = id
@@ -9,12 +12,42 @@ class Customer
   end
 
   def self.all
+
     customers = []
+    CSV.open("support/customers.csv", 'r').each do |line|
+      total = line.length
+      id = line[0].to_i
+      email = line[1]
+      address_array = line[2..total]
+      address_array.join
+      address = address_array
+      customers << self.new(id, email, address)
+    end
     return customers
   end
-
-  def self.find
-  end
-
-
+end
+#     customers_array = []
+#     customers = CSV.open("../support/customers.csv", 'r')#.each do |line|
+#       # customer_array = line.join
+#       # customer_array = line.split(",")
+#
+#         #customer_array.each do |id_email_address|
+#         customers.each do |id_email_address_to_hash|
+#
+#           customer_hash = {}
+#           customer_hash[:id] = customers_array[0]
+#           customer_hash[:email] = customers_array[1]
+#           customer_hash[:address] = customers_array[2]
+#
+#           customers_array << customer_hash
+#         end
+#     #end
+#
+#     return customers_array
+#   end
+#
+#   def self.find
+#   end
+#
+#
 end

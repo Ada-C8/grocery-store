bd713f2e02d32f854b9b3848d6a719026306b075
Eva Sawyer <esawyer25@gmail.com>
2017-08-24 22:24:44 -0700

    added a second test to initialize section

--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -1,27 +1,40 @@
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
-
+require_relative '../lib/customer'
 # TODO: uncomment the next line once you start wave 3
-# require_relative '../lib/customer'
+
 
 describe "Customer" do
   describe "#initialize" do
+    it "Can be created" do
+      customer_id = 13
+      email = "bert@yahoo.com"
+      address = "12 Caldwell Ln, Nashvegas, TN 43254"
+      person = Customer.new(customer_id, email, address)
+      person.must_be_instance_of Customer
+    end
+
     it "Takes an ID, email and address info" do
-      id = 1337
-      order = Grocery::Order.new(id, {})
+      # TODO: Your test code here!
+      customer_id = 13
+      email = "bert@yahoo.com"
+      address = "12 Caldwell Ln, Nashvegas, TN 43254"
+      person = Customer.new(customer_id, email, address)
 
-      order.must_respond_to :id
-      order.id.must_equal id
-      order.id.must_be_kind_of Integer
+      person.must_respond_to :customer_id
+      person.customer_id.must_equal customer_id
 
-      order.must_respond_to :products
-      order.products.length.must_equal 0
+      person.must_respond_to :email
+      person.email.must_equal email
+
+      person.must_respond_to :address
+      person.address.must_equal address
     end
   end
 
   describe "Customer.all" do
-    xit "Returns an array of all customers" do
+    it "Returns an array of all customers" do
       # TODO: Your test code here!
       # Useful checks might include:
       #   - Customer.all returns an array
@@ -34,16 +47,108 @@ describe "Customer" do
   end
 
   describe "Customer.find" do
-    xit "Can find the first customer from the CSV" do
+    it "Can find the first customer from the CSV" do
       # TODO: Your test code here!
     end
 
-    xit "Can find the last customer from the CSV" do
+    it "Can find the last customer from the CSV" do
       # TODO: Your test code here!
     end
 
-    xit "Raises an error for a customer that doesn't exist" do
+    it "Raises an error for a customer that doesn't exist" do
       # TODO: Your test code here!
     end
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# require 'minitest/autorun'
+# require 'minitest/reporters'
+# require 'minitest/skip_dsl'
+# require_relative '../lib/customer'
+#
+# # TODO: uncomment the next line once you start wave 3
+#
+#
+# describe "Customer" do
+#   describe "#initialize" do
+#     it "Can be created" do
+#       customer_id = 13
+#       email = "bert@yahoo.com"
+#       address = "12 Caldwell Ln, Nashvegas, TN 43254"
+#       person = Customer.new(customer_id, email, address)
+#       person.must_be_instance_of Customer
+#     end
+#   end
+#   # it "Takes an ID, email and address info" do
+#   #   customer_id = 13
+#   #   email = "bert@yahoo.com"
+#   #   address = "12 Caldwell Ln, Nashvegas, TN 43254"
+#   #   person = Customer.new(customer_id, email, address)
+#
+#   # person.must_respond_to :customer_id
+#   # person.customer_id.must_equal customer_id
+#   # person.customer_id.must_equal customer_id
+#   #
+#   # person.must_respond_to :email
+#   # person.email.must_equal email
+#   #
+#   # person.must_respond_to :address
+#   # person.address.must_equal address
+#   #
+#   #
+#   #
+#   #   xdescribe "Customer.all" do
+#   #     xit "Returns an array" do
+#   #       # TODO: Your test code here!
+#   #       all_customers = Customer.all
+#   #       all_customers.must_be_kind_of Array
+#   #     end
+#   #
+#   #     xit "Everything in the array is a Customer"
+#   #     all_customers = Customer.all
+#   #     all_customers.each do |customers|
+#   #       customers.must_be_kind_of Customer
+#   #     end
+#   #
+#   #
+#   #
+#   #
+#   #
+#   #     # Useful checks might include:
+#   #     #   - Customer.all returns an array
+#   #     #   - Everything in the array is a Customer
+#   #     #   - The number of customers is correct
+#   #     #   - The ID, email address of the first and last
+#   #     #       customer match what's in the CSV file
+#   #     # Feel free to split this into multiple tests if needed
+#   #   end
+#   #
+#   #
+#   #   xdescribe "Customer.find" do
+#   #     xit "Can find the first customer from the CSV" do
+#   #       # TODO: Your test code here!
+#   #     end
+#   #
+#   #     xit "Can find the last customer from the CSV" do
+#   #       # TODO: Your test code here!
+#   #     end
+#   #
+#   #     xit "Raises an error for a customer that doesn't exist" do
+#   #       # TODO: Your test code here!
+#   #     end
+#   #   end
+#   # end

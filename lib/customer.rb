a1e51db134bc84f3a9340d35f2cc96b1932a08d8
Lauren <Laurenelee88@gmail.com>
2017-08-24 17:58:51 -0700

    split up csv

--- lib/customer.rb
+++ lib/customer.rb
@@ -11,20 +11,23 @@ module Grocery
       @address = address
     end # initialize
 
-# 1,leonard.rogahn@hagenes.org,71596 Eden Route,Connellymouth,LA,98872-9105
+    # 1,leonard.rogahn@hagenes.org,71596 Eden Route,Connellymouth,LA,98872-9105
 
     def self.all
-      customers = []
+
       CSV.open("../support/customers.csv", 'r').each do |line|
         id = line[0].to_i
-        address = {}
-        address_arr = line[2].split','
-
-
-      return customers
+        email = line[1]
+        address = {address: line[2], city: line[3], state: line[4], zipcode: line[5]}
+        customers = [id, email, address]
+        return customers
+      end
     end # self.all
 
     def self.find(id)
     end # self.find
+
+
+
   end # Customer class
 end # Grocery module

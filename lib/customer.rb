e11bffb4ca207b1c9bb8597bf0dcbf80bd5ed32d
Sarah Read-Brown <srbusiness2@gmail.com>
2017-08-24 18:01:19 -0700

    Got the csv file to read and format need to work on self

--- lib/customer.rb
+++ lib/customer.rb
@@ -14,7 +14,13 @@ module  Grocery
     def self.all
       # return orders = [1]
       # returns a collection of Customer instances, representing all of the Customer described in the CSV. See below for the CSV file specifications
-
+      CSV.open("support/customers.csv", "r").each do |line|
+        id = line[0].to_i
+        email = line[1]
+        address = {address1: line[2], city: line[3], state: line[4], zip_code: line[5] }
+        array = [id, email, address]
+        return array
+      end
     end
 
     def self.find(id)

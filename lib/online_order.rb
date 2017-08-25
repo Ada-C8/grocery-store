18386ae9522680d0f57f56b538445b2bf21e9a14
Sarah Read-Brown <srbusiness2@gmail.com>
2017-08-25 12:33:17 -0700

    updates online.rb with note and skeleton framework for methods I will build.

--- lib/online_order.rb
+++ lib/online_order.rb
@@ -1,7 +1,29 @@
-# require 'pry'
-# require 'csv'
-#
-# class OnlineOrder < Order
-#
-#
-# end
+require 'pry'
+require 'csv'
+
+class OnlineOrder < Order
+
+  def initialize(id, products, customer_id, status)
+    super
+    @customer_id = customer_id
+    @status = status.to_sym
+  end
+
+  def self.all
+    #no super because super takes the out put for orders and gives back @@all_orders, in this case we dont care about that we want all the online orders
+  end
+
+  def find.all
+    super
+  end
+
+  def total
+    super
+  end
+
+  def add_product
+    super
+  end
+
+
+end
--- specs/online_order_spec.rb
+++ specs/online_order_spec.rb
@@ -12,7 +12,7 @@ require_relative '../lib/online_order'
 # we effectively get all that testing for free! Here we'll
 # only test things that are different.
 
-xdescribe "OnlineOrder" do
+describe "OnlineOrder" do
   describe "#initialize" do
     it "Is a kind of Order" do
       # Check that an OnlineOrder is in fact a kind of Order
@@ -31,7 +31,7 @@ xdescribe "OnlineOrder" do
     end
   end
 
-  describe "#total" do
+  xdescribe "#total" do
     it "Adds a shipping fee" do
       # TODO: Your test code here!
     end
@@ -41,7 +41,7 @@ xdescribe "OnlineOrder" do
     end
   end
 
-  describe "#add_product" do
+  xdescribe "#add_product" do
     it "Does not permit action for processing, shipped or completed statuses" do
       # TODO: Your test code here!
     end
@@ -51,7 +51,7 @@ xdescribe "OnlineOrder" do
     end
   end
 
-  describe "OnlineOrder.all" do
+  xdescribe "OnlineOrder.all" do
     it "Returns an array of all online orders" do
       # TODO: Your test code here!
       # Useful checks might include:
@@ -64,7 +64,7 @@ xdescribe "OnlineOrder" do
     end
   end
 
-  describe "OnlineOrder.find_by_customer" do
+  xdescribe "OnlineOrder.find_by_customer" do
     it "Returns an array of online orders for a specific customer ID" do
       # TODO: Your test code here!
     end

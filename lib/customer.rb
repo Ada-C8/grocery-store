85b030877166d337b094c9c0901ca2414987e292
Lauren <Laurenelee88@gmail.com>
2017-08-25 12:07:20 -0700

    changed id to customer_id in customer file

--- lib/customer.rb
+++ lib/customer.rb
@@ -2,11 +2,11 @@ require 'csv'
 
 module Grocery
   class Customer
-    attr_reader :id, :email, :address
+    attr_reader :customer_id, :email, :address
+@@customers = []
 
-
-    def initialize(id, email, address)
-      @id = id
+    def initialize(customer_id, email, address)
+      @customer_id = customer_id
       @email = email
       @address = address
     end # initialize
@@ -14,20 +14,30 @@ module Grocery
     # 1,leonard.rogahn@hagenes.org,71596 Eden Route,Connellymouth,LA,98872-9105
 
     def self.all
-      customers = []
+      # customers = []
+      if @@customers.length > 0
+        return @@customers
+      end
       CSV.open("../support/customers.csv", 'r').each do |line|
-        id = line[0].to_i
+        customer_id = line[0].to_i
         email = line[1]
         address = {address: line[2], city: line[3], state: line[4], zipcode: line[5]}
-        customers << self.new(id, email, address)
+        @@customers << self.new(customer_id, email, address)
       end
-      return customers
+      return @@customers
     end # self.all
 
-    def self.find(id)
-    end # self.find
+    def self.find(customer_id)
+      if customer_id > all.length
+        raise ArgumentError.new("Error: Customer #{customer_id} does not exist")
+      end
 
-
+      Customer.all.each do |order|
+        if order.customer_id == customer_id
+          return order
+        end
+      end
+    end # end self.find method
 
   end # Customer class
 end # Grocery module

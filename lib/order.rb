782cf307b9b8f5330a7491bc8451ce52d88bd4c3
Severin Wiggenhorn <severin.randall@gmail.com>
2017-08-24 18:48:54 -0700

    made variable names consistent across files

--- lib/order.rb
+++ lib/order.rb
@@ -1,82 +1,76 @@
 require 'csv'
 require 'pry'
 require 'awesome_print'
+# do i need to require_relative anything from customer and online_order here???
 
 module Grocery
   class Order
-    attr_reader :id, :products, :total, :all_orders, :orders
+    attr_reader :id, :food_and_price, :total, :all_orders, :orders
 
-    def initialize(id, products)
+    def initialize(id, food_and_price)
       @id = id
-      @products = products
+      @food_and_price = food_and_price
     end
-
 
 
     def self.all(file_name)
       @orders = []
       CSV.open(file_name, "r").each do |row|
-        @products = {}
+        @food_and_price = {}
         @id = row[0].to_i
         @product_list = row[1].split(";")
         @product_list.each do |sep|
           food_price_array = sep.split(":")
-          @products[food_price_array[0].to_s] = food_price_array[1].to_f
+          @food_and_price[food_price_array[0].to_s] = food_price_array[1].to_f
         end
-        @orders << Grocery::Order.new(@id, @products)
+        @orders << Grocery::Order.new(@id, @food_and_price)
       end
       return @orders
     end
 
-  def self.find(id)
-
-  end
+    def self.find(file_name, id)
+      self.all(file_name).each do |instance|
+        if instance.id == id
+          return instance
+        end
+      end
+      raise ArgumentError.new "Invalid Order Number"
+    end
 
     def total
       sum = 0
-      @products.values.each do |price|
+      @food_and_price.values.each do |price|
         sum += price
       end
       total = sum + (sum * 0.075).round(2)
     end
 
     def add_product(product_name, product_price)
-      unless @products.has_key?(product_name)
-        @products[product_name] = product_price
+      unless @food_and_price.has_key?(product_name)
+        @food_and_price[product_name] = product_price
         return true
       else
         return false
       end
     end
 
-def remove_product(product_name)
-  if @products.has_key?(product_name)
-    @products.delete(product_name)
-    return true
-  else
-    return false
+    def remove_product(product_name)
+      if @food_and_price.has_key?(product_name)
+        @food_and_price.delete(product_name)
+        return true
+      else
+        return false
+      end
+    end
   end
 end
 
-  end
-end
+#
+# all_the_orders = Grocery::Order.all("./support/orders.csv")
+# #
+# ap all_the_orders
 
-
-puts Grocery::Order.all("./support/orders.csv").class 
-
+# order = Grocery::Order.find("./support/orders.csv", 1)
 #
-# puts order1.total
-#
-# puts order1.products.length
-#
-#
-# order1.add_product("kiwi", 1.00)
-#
-# puts order1.products.length
-#
-#
-# puts order1.total
-#
-# order1.remove_product("banana", 1.50)
-#
-# puts order1.products.length
+# puts order.id
+# puts order.id

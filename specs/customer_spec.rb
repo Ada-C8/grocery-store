d5063e6d606971ca979c5386130f2d3a9454a8d4
Eva Sawyer <esawyer25@gmail.com>
2017-08-25 11:33:28 -0700

    added tests for customer.find

--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -11,8 +11,8 @@ describe "Customer" do
       customer_id = 13
       email = "bert@yahoo.com"
       address = "12 Caldwell Ln, Nashvegas, TN 43254"
-      person = Customer.new(customer_id, email, address)
-      person.must_be_instance_of Customer
+      person = Grocery::Customer.new(customer_id, email, address)
+      person.must_be_instance_of Grocery::Customer
     end
 
     it "Takes an ID, email and address info" do
@@ -20,7 +20,7 @@ describe "Customer" do
       customer_id = 13
       email = "bert@yahoo.com"
       address = "12 Caldwell Ln, Nashvegas, TN 43254"
-      person = Customer.new(customer_id, email, address)
+      person = Grocery::Customer.new(customer_id, email, address)
 
       person.must_respond_to :customer_id
       person.customer_id.must_equal customer_id
@@ -34,23 +34,20 @@ describe "Customer" do
   end
 
   describe "Customer.all" do
-    # TODO: Your test code here!
-    # Useful checks might include:
-    #   - Customer.all returns an array
     it "Returns an array of all customers" do
-      all_customers = Customer.all
+      all_customers = Grocery::Customer.all
       all_customers.must_be_kind_of Array
     end
 
     it "Everything in the Array is a Customer" do
-      all_customers = Customer.all
+      all_customers = Grocery::Customer.all
       all_customers.each do |single_customer|
-        single_customer.must_be_instance_of Customer
+        single_customer.must_be_instance_of Grocery::Customer
       end
     end
 
-    it "Id's of first and last customers match array" do
-      all_customers = Customer.all
+    it "Id's of first and last customers match csv" do
+      all_customers = Grocery::Customer.all
       csv = CSV.open("support/customers.csv", 'r')
       csv_id_first = csv.to_a[0][0]
       all_customers[0].customer_id.must_equal csv_id_first
@@ -62,36 +59,71 @@ describe "Customer" do
 
     end
 
-    it "The email of the first and last customers must match array" do
-      all_customers = Customer.all
-      cvs = CSV.open("support/customers.csv", 'r')
-      cvs_email_first = cvs.to_a[0][1]
-      all_customers[0].email.must_equal cvs_email_first
+    it "The email of first and last customers must match csv" do
+      all_customers = Grocery::Customer.all
+      csv = CSV.open("support/customers.csv", 'r')
+      csv_email_first = csv.to_a[0][1]
+      all_customers[0].email.must_equal csv_email_first
 
-      cvs = CSV.open("support/customers.csv", 'r')
-      cvs_email_last = cvs.to_a[-1][1]
-      all_customers[-1].email.must_equal cvs_email_last
+      csv = CSV.open("support/customers.csv", 'r')
+      csv_email_last = csv.to_a[-1][1]
+      all_customers[-1].email.must_equal csv_email_last
+    end
+    it "The address of first and last customers must match csv" do
+      all_customers = Grocery::Customer.all
+      csv = CSV.open("support/customers.csv", 'r')
+      csv_address_first = csv.to_a[0][2..5]
+      all_customers[0].address.must_equal csv_address_first
+
+      csv = CSV.open("support/customers.csv", 'r')
+      csv_address_last = csv.to_a[-1][2..5]
+      all_customers[-1].address.must_equal csv_address_last
     end
 
     it "The number of orders is correct" do
-      all_customers = Customer.all
-      cvs = CSV.open("support/customers.csv", 'r')
-      all_customers.length.must_equal cvs.to_a.length
+      all_customers = Grocery::Customer.all
+      csv = CSV.open("support/customers.csv", 'r')
+      all_customers.length.must_equal csv.to_a.length
     end
 
   end
 
   describe "Customer.find" do
     it "Can find the first customer from the CSV" do
-      # TODO: Your test code here!
+      first_customer = Grocery::Customer.find(1)
+      csv = CSV.open("support/customers.csv", 'r')
+      csv_id_first = csv.to_a[0][0]
+      first_customer.customer_id.must_equal csv_id_first
+
+      csv = CSV.open("support/customers.csv", 'r')
+      csv_email_first = csv.to_a[0][1]
+      first_customer.email.must_equal csv_email_first
+
+      csv = CSV.open("support/customers.csv", 'r')
+      csv_address_first = csv.to_a[0][2..5]
+      first_customer.address.must_equal csv_address_first
     end
 
     it "Can find the last customer from the CSV" do
-      # TODO: Your test code here!
+      last_customer = Grocery::Customer.find(35)
+      csv = CSV.open("support/customers.csv", 'r')
+      csv_id_last = csv.to_a[34][0]
+      last_customer.customer_id.must_equal csv_id_last
+
+      csv = CSV.open("support/customers.csv", 'r')
+      csv_email_last = csv.to_a[34][1]
+      last_customer.email.must_equal csv_email_last
+
+      csv = CSV.open("support/customers.csv", 'r')
+      csv_address_last = csv.to_a[34][2..5]
+      last_customer.address.must_equal csv_address_last
     end
 
     it "Raises an error for a customer that doesn't exist" do
-      # TODO: Your test code here!
+      proc {Grocery::Customer.find(1000)}.must_raise ArgumentError
+
+
+
     end
   end
 end

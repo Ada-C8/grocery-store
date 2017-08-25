4979efe7a2b2ffa7978f81364d777d1d86e7ae70
murog <guillermina.muro@gmail.com>
2017-08-25 12:43:21 -0700

    first test working, Online order is an Order

---
+++ lib/online_order.rb
@@ -0,0 +1,6 @@
+require_relative 'order'
+require_relative 'customer'
+module Grocery
+  class OnlineOrder < Grocery::Order
+  end
+end
--- specs/online_order_spec.rb
+++ specs/online_order_spec.rb
@@ -3,7 +3,7 @@ require 'minitest/reporters'
 require 'minitest/skip_dsl'
 
 # TODO: uncomment the next line once you start wave 3
-# require_relative '../lib/online_order'
+require_relative '../lib/online_order'
 # You may also need to require other classes here
 
 # Because an OnlineOrder is a kind of Order, and we've
@@ -11,7 +11,7 @@ require 'minitest/skip_dsl'
 # we effectively get all that testing for free! Here we'll
 # only test things that are different.
 
-xdescribe "OnlineOrder" do
+describe "OnlineOrder" do
   describe "#initialize" do
     it "Is a kind of Order" do
       result = Grocery::OnlineOrder.is_a? Grocery::Order
@@ -43,103 +43,103 @@ xdescribe "OnlineOrder" do
       # TODO: Your test code here!
     end
   end
-
-  describe "#total" do
-    it "Adds a shipping fee" do
-      products = {"carrots" => 1.99, "salad mix" => 3.99}
-      regular_order = Grocery::Order.new(200, products)
-      online_order = Grocery::OnlineOrder.new(200, products, 1, :complete)
-      regular_total = regular_order.total
-      expected_online_total = regular_total + 10
-      online_order.total.must_equal expected_online_total
-    end
-
-    it "Doesn't add a shipping fee if there are no products" do
-      empty_products = {}
-      online_order = Grocery::OnlineOrder.new(200, empty_products,1,:completed)
-      expected_online_total = 0
-      actual_online_total = online_order.total
-      actual_online_total.must_equal expected_online_total
-    end
-  end
-
-  describe "#add_product" do
-    it "Does not permit action for processing, shipped or completed statuses" do
-      products = {"carrots" => 1.99, "salad mix" => 3.99}
-      completed_online_order = Grocery::OnlineOrder.new(200, products,1, :completed)
-      shipped_online_order = Grocery::OnlineOrder.new(201, products,1, :shipped)
-      processing_online_order = Grocery::OnlineOrder.new(202, products,1,:processing)
-
-      proc {completed_online_order.add_product("almond butt", 7.99)}.must_raise ArgumentError
-      proc {shipped_online_order.add_product("almond butt", 8.99)}.must_raise ArgumentError
-      proc {processing_online_order.add_product("almond butt", 9.99)}.must_raise ArgumentError
-    end
-
-    it "Permits action for pending and paid satuses" do
-      products = {"carrots" => 1.99, "salad mix" => 3.99}
-      pending_online_order = Grocery::OnlineOrder.new(200, products, 1, :pending)
-      paid_online_order = Grocery::OnlineOrder.new(201, products, 1, :paid)
-      expected_count = 3
-      pending_online_order.products.count.must_equal expected_count
-      paid_online_order.products.count.must_equal expected_count
-    end
-  end
-
-  describe "OnlineOrder.all" do
-    it "Returns an array" do
-      result = Grocery::OnlineOrder.all
-      result.must_be_instance_of Array
-    end
-    it "Everything in the array is an Order" do
-      result = Grocery::OnlineOrder.all
-      result.each do |element|
-        element.must_be_instance_of Grocery::Order
-      end
-    end
-    it "The number of order is correct" do
-      result = Grocery::OnlineOrder.all
-      result.length.must_equal 100
-    end
-    it "The customer is present" do
-      result = Grocery::OnlineOrder.all
-      result.each do |order|
-        has_customer = (1..35).include? order.id
-        has_customer.must_equal true
-      end
-
-    end
-    it "The status is present" do
-      result = Grocery::OnlineOrder.all
-      result.each do |order|
-        has_status = [:pending, :paid, :shipped, :completed].include? order.status
-        has_status.must_equal true
-      end
-    end
-
-  end
-  describe "OnlineOrder.find" do
-    it "Returns an order" do
-      result = Grocery::OnlineOrder.find(1)
-      result.must_be_instance_of Grocery::Order
-    end
-    it "Returns an error if Order does not exist" do
-      proc {Grocery::OnlineOrder.find(1000000)}.must_raise ArgumentError
-    end
-  end
-
-  describe "OnlineOrder.find_by_customer" do
-    it "Returns an array" do
-      result = Grocery::OnlineOrder.find_by_customer(1)
-      result.must_be_instance_of Array
-    end
-    it "Returns an array of online orders for a specific customer ID" do
-      actual_result = Grocery::OnlineOrder.find_by_customer(1)
-      expected_result = #TODO whatever customer 1 bought
-      actual_result.must_equal expected_result
-    end
-    it "Returns an error if customer doesn't exist"
-      proc {Grocery::OnlineOrder.find_by_customer(10000)}.must_raise ArgumentError
-    end
+  # 
+  # xdescribe "#total" do
+  #   it "Adds a shipping fee" do
+  #     products = {"carrots" => 1.99, "salad mix" => 3.99}
+  #     regular_order = Grocery::Order.new(200, products)
+  #     online_order = Grocery::OnlineOrder.new(200, products, 1, :complete)
+  #     regular_total = regular_order.total
+  #     expected_online_total = regular_total + 10
+  #     online_order.total.must_equal expected_online_total
+  #   end
+  #
+  #   it "Doesn't add a shipping fee if there are no products" do
+  #     empty_products = {}
+  #     online_order = Grocery::OnlineOrder.new(200, empty_products,1,:completed)
+  #     expected_online_total = 0
+  #     actual_online_total = online_order.total
+  #     actual_online_total.must_equal expected_online_total
+  #   end
+  # end
+  #
+  # xdescribe "#add_product" do
+  #   it "Does not permit action for processing, shipped or completed statuses" do
+  #     products = {"carrots" => 1.99, "salad mix" => 3.99}
+  #     completed_online_order = Grocery::OnlineOrder.new(200, products,1, :completed)
+  #     shipped_online_order = Grocery::OnlineOrder.new(201, products,1, :shipped)
+  #     processing_online_order = Grocery::OnlineOrder.new(202, products,1,:processing)
+  #
+  #     proc {completed_online_order.add_product("almond butt", 7.99)}.must_raise ArgumentError
+  #     proc {shipped_online_order.add_product("almond butt", 8.99)}.must_raise ArgumentError
+  #     proc {processing_online_order.add_product("almond butt", 9.99)}.must_raise ArgumentError
+  #   end
+  #
+  #   it "Permits action for pending and paid satuses" do
+  #     products = {"carrots" => 1.99, "salad mix" => 3.99}
+  #     pending_online_order = Grocery::OnlineOrder.new(200, products, 1, :pending)
+  #     paid_online_order = Grocery::OnlineOrder.new(201, products, 1, :paid)
+  #     expected_count = 3
+  #     pending_online_order.products.count.must_equal expected_count
+  #     paid_online_order.products.count.must_equal expected_count
+  #   end
+  # end
+  #
+  # xdescribe "OnlineOrder.all" do
+  #   it "Returns an array" do
+  #     result = Grocery::OnlineOrder.all
+  #     result.must_be_instance_of Array
+  #   end
+  #   it "Everything in the array is an Order" do
+  #     result = Grocery::OnlineOrder.all
+  #     result.each do |element|
+  #       element.must_be_instance_of Grocery::Order
+  #     end
+  #   end
+  #   it "The number of order is correct" do
+  #     result = Grocery::OnlineOrder.all
+  #     result.length.must_equal 100
+  #   end
+  #   it "The customer is present" do
+  #     result = Grocery::OnlineOrder.all
+  #     result.each do |order|
+  #       has_customer = (1..35).include? order.id
+  #       has_customer.must_equal true
+  #     end
+  #
+  #   end
+  #   it "The status is present" do
+  #     result = Grocery::OnlineOrder.all
+  #     result.each do |order|
+  #       has_status = [:pending, :paid, :shipped, :completed].include? order.status
+  #       has_status.must_equal true
+  #     end
+  #   end
+  #
+  # end
+  # xdescribe "OnlineOrder.find" do
+  #   it "Returns an order" do
+  #     result = Grocery::OnlineOrder.find(1)
+  #     result.must_be_instance_of Grocery::Order
+  #   end
+  #   it "Returns an error if Order does not exist" do
+  #     proc {Grocery::OnlineOrder.find(1000000)}.must_raise ArgumentError
+  #   end
+  # end
+  #
+  # xdescribe "OnlineOrder.find_by_customer" do
+  #   it "Returns an array" do
+  #     result = Grocery::OnlineOrder.find_by_customer(1)
+  #     result.must_be_instance_of Array
+  #   end
+  #   it "Returns an array of online orders for a specific customer ID" do
+  #     actual_result = Grocery::OnlineOrder.find_by_customer(1)
+  #     expected_result = #TODO whatever customer 1 bought
+  #     actual_result.must_equal expected_result
+  #   end
+  #   it "Returns an error if customer doesn't exist"
+  #     proc {Grocery::OnlineOrder.find_by_customer(10000)}.must_raise ArgumentError
+  #   end
 
 
 end

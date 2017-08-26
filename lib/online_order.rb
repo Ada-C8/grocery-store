ba16c9619ca7551c201f35b88615cbb415f545dc
Sarah Read-Brown <srbusiness2@gmail.com>
2017-08-26 13:47:30 -0700

    Added online orders.all method that passes spec tests

--- lib/online_order.rb
+++ lib/online_order.rb
@@ -6,15 +6,33 @@ module Grocery
   class OnlineOrder < Order
 
     attr_reader :customer_id, :status
-
+    @@all_online_orders = []
     def initialize(id, products, customer_id, status="pending" )
       super(id, products)
       @customer_id = customer_id
       @status = status.to_sym
     end
 
+    #no super because super takes the out put for orders and gives back @@all_orders, in this case we dont care about that we want all the online orders
+
+    #1,Lobster:17.18;Annatto seed:58.38;Camomile:83.21,25,complete
     def self.all
-      #no super because super takes the out put for orders and gives back @@all_orders, in this case we dont care about that we want all the online orders
+      if @@all_online_orders.length > 0
+        return @@all_online_orders
+      end
+      CSV.open("support/online_orders.csv", "r").each do |line|
+        id = line[0].to_i
+        customer_id = line[2]
+        status = line[3]
+        products_hash = {}
+        products_prices = line[1].split(';')
+        products_prices.each do |product|
+          ind_product_price = product.split(':')
+          products_hash[ind_product_price[0]] = ind_product_price[1].to_f
+        end
+        @@all_online_orders << self.new(id, products_hash, customer_id, status)
+      end
+      return @@all_online_orders
     end
 
     def self.find

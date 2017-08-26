90703bde1d99cfe1890fdbedc8f560dfe739b6fd
Jocelyn Gonzalez <jocegonz@gmail.com>
2017-08-26 13:08:28 -0700

    complete Customer.all

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,8 +1,8 @@
 require 'csv'
-require_relative 'order'
+# NO NEED FOR  ANY require_relative
 module Grocery
 
-  class Customer < Order
+  class Customer
     attr_reader :id, :email, :address
 
     def initialize(id, email, address)
@@ -11,42 +11,30 @@ module Grocery
       @address = address
     end
 
+    #CSV must be in an array
+    #in order to test using rake, only one period (instead of two) is needed
+    def self.all
+      cust_array = []
+      CSV.open('../support/customers.csv', 'r').each do |line|    ##
+        id = line.shift.to_i
+        email = line.shift
+        address = line
+        # hash = {}
+        # hash[:id] = @cust_id
+        # hash[:email] = @email
+        # hash[:address] = @address
+        cust_array << Grocery::Customer.new(id, email, address)
+      end
+      #returns object memory address
+      return cust_array
+    end
+
+    def self.find(id)
+      #returns instance of customer found
+      #by customer cust_id
+      #shouldn't need to change if CSV has
+      #already been changed
+    end
+
   end
-
 end
-
-
-
-# puts Customer.ancestors  #temp, checks its inheritance
-#
-#     #CSV must be in an array
-#     #in order to test using rake, only one period (instead of two) is needed
-#     def self.all
-#       array = []
-#       CSV.open('../support/customers.csv', 'r').each do |line|    ##
-#         @id = line.shift.to_i
-#         @email = line.shift
-#         @delivery_info = line
-#         return "#{@id}, #{@email}, #{@delivery_info}"
-#       end
-#       #returns object memory address
-#       return "#{@id}, #{@email}, #{@delivery_info}"
-#       #returns all customer info from new CSV
-#
-#       #if I don't put anything here
-#       #it should automatically be inherited
-#       #if I add something, super first
-#       #CSV specs need to be adjusted
-#     end
-#
-#     def self.find(id)
-#       #returns instance of customer found
-#       #by customer id
-#       #shouldn't need to change if CSV has
-#       #already been changed
-#     end
-#   end
-# end
-#
-# orders = Grocery::Order::Customer.self.all
-# print orders

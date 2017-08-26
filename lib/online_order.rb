659f370ba8ec9c9bcd0b334d1ba577d2597387e1
Amy Cash <amymcash@gmail.com>
2017-08-26 16:36:17 -0700

    Update all to add Customer Info

--- lib/online_order.rb
+++ lib/online_order.rb
@@ -1,9 +1,3 @@
-# 1,Lobster:17.18;Annatto seed:58.38;Camomile:83.21,25,complete
-# 2,Sun dried tomatoes:90.16;Mastic:52.69;Nori:63.09;Cabbage:5.35,10,paid
-# 3,Vegetable spaghetti:37.83;Dates:90.88;WhiteFlour:3.24;Caraway Seed:54.29,5,processing
-# 4,Aubergine:56.71;Brown rice vinegar:33.52;dried Chinese Broccoli:51.74,35,paid
-# 5,Sour Dough Bread:35.11,21,shipped
-# 6,Peaches:46.34,14,pending
 #include Grocery
 require_relative 'order'
 require_relative 'customer'
@@ -16,8 +10,12 @@ class OnlineOrder < Grocery::Order
 
   def initialize(id, products, customer_id, status)
     super(id, products)
-    @customer_id = customer_id.to_i
-    @status = status
+    @customer_id = customer_id
+    @status_array = [:pending, :paid, :processing, :complete]
+    if @status_array.include?(status.downcase)
+      @status = status
+    else @status = :pending
+    end
   end #init
 
   def total
@@ -27,57 +25,58 @@ class OnlineOrder < Grocery::Order
 
   def self.all_online
     products = {}
+    @@all_online_orders = []
 
     CSV.open('../support/online_orders.csv', 'r').each do |line|
       id = line[0].to_i #assign ids
       customer_id = line[2].to_i
       status = line[3].to_sym
-
-      x = line[1].split(';') #split out products by dividing by semi colons
-      #puts "x is #{x} and it is class #{x.class}"
-
-      x.each do |item|
-        y = item.split(":") #split prices from products
-        #puts "y is #{y} and it is #{y.class}\n\n"
-        x.length.times do
-          products[y[0]] = y[1] #assign prices to products in the products hash
-          #puts "In the middle of the length times do "
+      semi_colon_split = line[1].split(';') #split out products by dividing by semi colons
+      semi_colon_split.each do |item|
+        colon_split = item.split(":") #split prices from products
+        semi_colon_split.length.times do
+          products[colon_split[0]] = colon_split[1] #assign prices to products in the products hash
         end # length.times do
       end #of x.each do
-      @@all_online_orders << OnlineOrder.new(id, products, customer_id, status)
+      #@@all_online_orders << OnlineOrder.new(id, products, customer_id, status)
+      @@all_online_orders << OnlineOrder.new(id, products, Grocery::Customer.find(customer_id), status)
+
+      #(12, "amy@this.com", "123 Fake St., Dayton, Ohio, 12121")
       products = {}
     end #
     return @@all_online_orders
   end #all method
+
+  def add_product(product_name, product_price, status)
+    status = status.downcase.to_sym
+    if status == :paid || status == :pending
+      puts "That worked "
+    else
+      raise ArgumentError
+    end #if statment
+  end #add_product
+#   The add_product method should be updated to permit a new product to be added ONLY if the status is either pending or paid (no other statuses permitted)
+# Otherwise, it should raise an ArgumentError (Google this!)
 end #class
 
 
-puts OnlineOrder.all_online.inspect
 
+j = OnlineOrder.all_online
 
+j.each do |order|
+  puts "Customer id is #{order.customer_id.inspect}"
+end
 
-# def self.all
-#   products = {}
-#   CSV.open('../support/orders.csv', 'r').each do |line|
-#     id = line[0].to_i #assigns first # in CSV to be the order ID
-#     x = line[1].split(';') #splits the orders by semicolons
-#     x.each do |item|
-#       y = item.split(":") #seperates product name and price
-#       x.length.times do
-#         products[y[0]] = y[1].to_f #adds product name and price to products hash
-#       end
-#     end #of x.each do
-#     @@all_online_orders << Grocery::Order.new(id, products) #initalizes a new order using the id and products hash for the current line and adds it to all orders array
-#     products = {} #clears the products array for the next order.
-#   end #of CSV line by line
-#   return @@all_online_orders
 # end
-# x = OnlineOrder.new(19, {cheese:5.00, bacon:5.00}, Grocery::Customer.new(12, "amy@this.com", "123 Fake St., Dayton, Ohio, 12121"), :pending)
-# puts x.customer_id
-# puts x.products
-# puts x.inspect
-# puts x.total
-#puts x.total
+# x = OnlineOrder.new(19, {cheese:5.00, bacon:5.00}, Grocery::Customer.new(12, "amy@this.com", "123 Fake St., Dayton, Ohio, 12121"), :christmas)
+# puts "#{x }"
+# puts "id is #{x.id}"
+# puts "Products are #{x.products}"
+# puts "Customer id is #{x.customer_id} and it is class #{x.customer_id.class}"
+# puts "Status is #{x.status}"
+# puts "Total is #{x.total}"
+
+
 # A customer object
 # A fulfillment status (stored as a Symbol)
 # pending, paid, processing, shipped or complete

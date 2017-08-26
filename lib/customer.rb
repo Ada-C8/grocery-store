56094d276530be95d35ac0f201b800233f4a528f
Eva Sawyer <esawyer25@gmail.com>
2017-08-26 12:03:24 -0700

    Cleaned up code and added comments

--- lib/customer.rb
+++ lib/customer.rb
@@ -10,28 +10,35 @@ module Grocery
       @address = address
     end
 
+
     def self.all
-      #csv_array_of_all_orders = []
-      #1: Read the csv file
-      #cvs line = id_number, email, housenumber street,town, state, zipcode
-      #make into [(id_number, email, (address))]
+      #csv line format =
+      #id_number,email,housenumber street,town,state,zipcode
+      #Goal = ([(id_number, email, (address))], ... )
+      #1. Open file and extract id_number, email and address
       cust_info_arr = []
       CSV.open("support/customers.csv", 'r').each do |line|
         one_customer =[]
         customer_id = line[0]
         email = line[1]
         address = line[2..5]
+        #2. put information for a single customer into a array
         one_customer << customer_id << email << address
+        #3. put information for all customers into a array
         cust_info_arr << one_customer
       end
-      customers = []
 
+      all_customers = []
+     #4. make new instances of Customer for each object in array in 3
       cust_info_arr.each do |single_cust_info|
-        customers << Customer.new(single_cust_info[0], single_cust_info[1], single_cust_info[2])
+        new_customer = Customer.new(single_cust_info[0], single_cust_info[1], single_cust_info[2])
+        all_customers << new_customer
       end
-      return customers
+      #5. return an array of all customers
+      return all_customers
     end
 
+    #returns an instance of Customer where the value of the id field in the CSV matches the passed parameter.
     def self.find(customer_id)
       collection_of_customers = Customer.all
       collection_of_customers.each do |customer|
@@ -41,16 +48,5 @@ module Grocery
       end
       raise ArgumentError.new("Customer number #{customer_id} could not be found.")
     end
-
-
   end #end of customer class
 end
-
-# all_customers = Customer.all
-# puts all_customers.class
-# puts all_customers[0].class
-# customer_id = 13
-# email = "bert@yahoo.com"
-# address = "12 Caldwell Ln, Nashvegas, TN 43254"
-# person = Customer.new(customer_id, email, address)
-# puts person.address

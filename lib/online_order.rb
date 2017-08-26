a40d448643aae89e44b5b143ae112599410ed8f9
Sarah Read-Brown <srbusiness2@gmail.com>
2017-08-26 15:42:09 -0700

    Adds online order find by customer method that passes tests

--- lib/online_order.rb
+++ lib/online_order.rb
@@ -9,6 +9,7 @@ module Grocery
     @@all_online_orders = []
     def initialize(id, products, customer_id, status="pending" )
       super(id, products)
+      #  @customer = Grocery::Customer.find(customer_id) #so that we use an instance of the Customer class instead of just the customer_id in the csv file to access data about each customer
       @customer_id = customer_id
       @status = status.to_sym
     end
@@ -33,12 +34,32 @@ module Grocery
       return @@all_online_orders
     end
 
-    def self.find
-      super
+    def self.find(id_input)
+      counter = 0
+      OnlineOrder.all.each do |order|
+        if order.id == id_input
+          counter += 1
+          return order
+        end
+      end
+      if counter == 0
+        raise ArgumentError.new("Invalid order number - order does not exist")
+      end
     end
 
-    def self.find_by_customer
-
+    def self.find_by_customer(customer_id_input)
+      counter = 0
+      orders_array =[]
+      OnlineOrder.all.each do |order|
+        if order.customer_id == customer_id_input.to_s
+          counter += 1
+          orders_array << order
+        end
+      end
+      if counter == 0
+        raise ArgumentError.new("Invalid customer ID- customer does not exist")
+      end
+      return orders_array
     end
 
     def total
@@ -54,7 +75,7 @@ module Grocery
       when :paid, :pending
         super(product, price)
       when :complete, :processing, :shipped
-        raise ArgumentError.new("Error - Cannot add new products at this point in your order.")
+        raise ArgumentError.new("Error - You cannot add new products at this point in you order because your order status is #{status.to_s} .")
       end
     end
   end

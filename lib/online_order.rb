7e8877012ed6dae940faf54c946987b059d5e20b
Bennett Rahn <rahnibennett@gmail.com>
2017-08-25 21:57:13 -0700

    update .all method -- has not been DRYed up.

--- lib/online_order.rb
+++ lib/online_order.rb
@@ -3,7 +3,9 @@ module Grocery
   class OnlineOrder < Order
     attr_reader :customer_id, :status
 
-    def initialize(id, products, customer_id, status)
+    @@online_orders = []
+
+    def initialize(id, products, customer_id, status = :pending)
       super(id, products)
       @customer_id = customer_id
       @status = status.to_sym
@@ -20,42 +22,40 @@ module Grocery
     end
 
     def add_product(product_name, product_price)
-      #if the order status is processing, shipped, completed it won't do anything
-      #how do you test the status?
+      #if the order status is processing, shipped, complete it won't do anything
       case @status
-      when :processing, :shipped, :completed
+      when :processing, :shipped, :complete
         return false
       when :pending, :paid
         super
-      # else
-      #   return "lalalala"
+        # else
+        #   return "lalalala"
       end
     end
 
-    # def self.all
-    #   # orders = []
-    #   if @@orders.length > 0
-    #     return @@orders
-    #   end
-    #   CSV.open("support/orders.csv", 'r').each do |line|
-    #     id = line[0].to_i
-    #     # line[1] = line[1].split(';')
-    #     # line[1] = line[1].gsub!':' '=>'
-    #     # products = line[1].to_h
-    #     # products = line[1]
-    #     products_hash = {}
-    #     products_arr = line[1].split';'
-    #     products_arr.each do |item_colon_price|
-    #       product_price = item_colon_price.split':'
-    #       products_hash[product_price[0]] = product_price[1].to_f
-    #
-    #     end
-    #
-    #     @@orders << self.new(id, products_hash)
-    #
-    #   end
-    #   return @@orders
-    # end
+    def self.all
+      # orders = []
+      if @@online_orders.length > 0
+        return @@online_orders
+      end
+      CSV.open("support/online_orders.csv", 'r').each do |line|
+        id = line[0].to_i
+
+        products_hash = {}
+        products_arr = line[1].split';'
+        products_arr.each do |item_colon_price|
+          product_price = item_colon_price.split':'
+          products_hash[product_price[0]] = product_price[1].to_f
+
+        end
+        customer_id = line[2].to_i
+        status = line[3].to_sym
+
+        @@online_orders << self.new(id, products_hash, customer_id, status)
+
+      end
+      return @@online_orders
+    end
 
 
     def self.find(id)
@@ -75,8 +75,8 @@ module Grocery
       # return found_customer
     end
 
-    def customer(customer_id)
-      customer = Grocery::Customer.find(customer_id)
+    def customer
+      customer = Grocery::Customer.find(@customer_id)
       return customer
     end
   end
@@ -86,20 +86,11 @@ end
 
 
 
-# Each new OnlineOrder should include all attributes from the `Order` class as well as the following **additional** attributes:
-# - A customer object
-# - A fulfillment status (stored as a **Symbol**)
-#   - pending, paid, processing, shipped or complete
-#   - If no status is provided, it should set to pending as the default
-#
-# The OnlineOrder should include the following updated functionality:
-# - The `total` method should be the same, except it will add a $10 shipping fee
 # - The `add_product` method should be updated to permit a new product to be added **ONLY** if the status is either pending or paid (no other statuses permitted)
 #   - Otherwise, it should raise an `ArgumentError` (Google this!)
-#
-# The OnlineOrder should also have the following **class** methods:
-#   - `self.all` - returns a collection of `OnlineOrder` instances, representing all of the OnlineOrders described in the CSV. See below for the CSV file specifications
-#     - **Question** Ask yourself, what is different about this `all` method versus the `Order.all` method? What is the same?
+
+
+
 #   - `self.find(id)` - returns an instance of `OnlineOrder` where the value of the id field in the CSV matches the passed parameter.
 #     -**Question** Ask yourself, what is different about this `find` method versus the `Order.find` method?
 #   - `self.find_by_customer(customer_id)` - returns a **list** of `OnlineOrder` instances where the value of the customer's ID matches the passed parameter.

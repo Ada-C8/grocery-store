21b295037fcadfdaac681874383320723ea21b55
Laura Robertson <mslaura.mae@gmail.com>
2017-08-25 22:10:56 -0700

    3 Tests written

--- specs/online_order_spec.rb
+++ specs/online_order_spec.rb
@@ -3,7 +3,7 @@ require 'minitest/reporters'
 require 'minitest/skip_dsl'
 
 # TODO: uncomment the next line once you start wave 3
-# require_relative '../lib/online_order'
+require_relative '../lib/online_orders'
 # You may also need to require other classes here
 
 # Because an OnlineOrder is a kind of Order, and we've
@@ -11,22 +11,28 @@ require 'minitest/skip_dsl'
 # we effectively get all that testing for free! Here we'll
 # only test things that are different.
 
-xdescribe "OnlineOrder" do
+describe "OnlineOrder" do
   describe "#initialize" do
     it "Is a kind of Order" do
       # Check that an OnlineOrder is in fact a kind of Order
 
-      # Instatiate your OnlineOrder here
-      # online_order =
-      # online_order.must_be_kind_of Grocery::Order
+      #Instatiate your OnlineOrder here
+      id = 1337
+      products = { "banana" => 1.99, "cracker" => 3.00, "sandwich" => 4.25 }
+      online_order = Grocery::Order.new(id,products)
+      online_order.must_be_kind_of Grocery::Order
     end
 
     it "Can access Customer object" do
       # TODO: Your test code here!
+      Grocery::OnlineOrder.all[5].customer.must_be_instance_of Integer
+      Grocery::OnlineOrder.find(10).customer.must_equal 26
     end
 
     it "Can access the online order status" do
       # TODO: Your test code here!
+      Grocery::OnlineOrder.all[10].status.must_be_instance_of String
+      Grocery::OnlineOrder.find(17).status.must_equal "processing"
     end
   end
 
@@ -62,7 +68,7 @@ xdescribe "OnlineOrder" do
       # Feel free to split this into multiple tests if needed
     end
   end
-  
+
   describe "OnlineOrder.find_by_customer" do
     it "Returns an array of online orders for a specific customer ID" do
       # TODO: Your test code here!

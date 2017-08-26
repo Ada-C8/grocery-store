01f1037c6ab5ed3218a66c4f0d56722cdac78eb4
Lauren <Laurenelee88@gmail.com>
2017-08-26 14:23:12 -0700

    wrote .all method

--- lib/online_order.rb
+++ lib/online_order.rb
@@ -4,7 +4,9 @@ require_relative 'order'
 module Grocery
   class OnlineOrder < Order
     attr_reader :customer_id, :status
-    def initialize(id, products, customer_id, status)
+    @@all_online_orders = []
+
+    def initialize(id, products, customer_id, status = :pending)
       super(id, products)
       @customer_id = customer_id
       @status = status.to_sym
@@ -33,7 +35,29 @@ module Grocery
 
 
     def self.all
+      if @@all_online_orders.length > 0
+        return @@all_online_orders
+      end
       # not using super b/c new csv and taking in more arguments (we don't want anything from Order.all from Order file) we want ALL of the online_orders
+      CSV.open("support/online_orders.csv", 'r').each do |line|
+        id = line[0].to_i
+        customer_id = line[2].to_i
+        status = line[3].to_sym
+        # playing with .sub!
+        # line[1] = line[1].gsub! ';', ','
+        # line[1] = line[1].gsub! ':', '=>'
+        products = {}
+        products_arr = line[1].split(';')
+        products_arr.each do |item|
+          product_price = item.split(':')
+          products[product_price[0]] = product_price[1].to_f
+        end
+
+
+        @@all_online_orders << self.new(id, products, customer_id, status)
+
+      end # each loop
+      return @@all_online_orders
     end #self.all
 
     def self.find(id)

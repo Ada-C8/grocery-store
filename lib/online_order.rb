fc355baaf9faf16863f00b16c2f3faf652093a8f
Kimberley Zell <kimberley.zell@gmail.com>
2017-08-26 16:24:38 -0700

    Added self.all and self.find methods

--- lib/online_order.rb
+++ lib/online_order.rb
@@ -12,6 +12,7 @@
 
 # The add_product method should be updated to permit a new product to be added ONLY if the status is either pending or paid (no other statuses permitted)
 # Otherwise, it should raise an ArgumentError (Google this!)
+
 # The OnlineOrder should also have the following class methods:
 #
 # self.all - returns a collection of OnlineOrder instances, representing all of the OnlineOrders described in the CSV. See below for the CSV file specifications
@@ -24,29 +25,68 @@ require 'csv'
 require 'pry'
 
 module Grocery
-class OnlineOrder < Order
-  attr_reader :customer_id
-  attr_accessor :status
-  def initialize(id, products, customer_id, status = :pending)
-    super(id, products)
-    @customer_id = customer_id
-    @status = status.to_sym
+  class OnlineOrder < Order
+    attr_reader :customer_id
+    attr_accessor :status
+    def initialize(id, products, customer_id, status = :pending)
+      super(id, products)
+      @customer_id = customer_id
+      @status = status.to_sym
+    end
+
+    def self.all
+      @online_orders = []
+      CSV.open("support/online_orders.csv", 'r').each do |line|
+        #line.split(",")
+
+        id = line[0].to_i
+        customer_id = line[2].to_i
+        status = line[3].to_sym
+        products_hash = {}
+        products_arr = line[1].split';'
+
+        products_arr.each do |item_and_price|
+          product_price = item_and_price.split':'
+          products_hash[product_price[0]] = product_price[1].to_f
+        end
+        @online_orders << self.new(id, products_hash, customer_id, status)
+      end
+      return @online_orders
+    end
+
+    def self.find_by_customer(val)
+      all_online_orders = Grocery::OnlineOrder.all
+
+      customer_order = nil
+      customer_orders = []
+
+      all_online_orders.each do |order|
+        if order.customer_id == val
+          customer_order = order
+          customer_orders << customer_order
+        end
+      end
+      if customer_order == nil
+        raise ArgumentError.new("Invalid order #{val}")
+      else
+        return customer_orders
+      end
+    end
+
+
+    def total
+      super + 10 #adds $10 shipping fee
+    end
+
+    #This doesn't work yet. Once have self.all method, try updating and try again
+    def add_product(product_name, product_price)
+      unless Grocery::OnlineOrder.status == :pending
+        raise ArgumentError.new("Due to order status, additional products can no longer be added to the order.")
+      end
+      super(product_name, product_price)
+
+    end
+
   end
-
-  def total
-    super + 10 #adds $10 shipping fee
-  end
-
-  def add_product(product_name, product_price)
-    # 
-    # super(product_name, product_price)
-    # unless Grocery::OnlineOrder.status == :pending
-    #   raise ArgumentError.new("Due to order status, additional products can no longer be added to the order.")
-    # end
-
-
-  end
-
-end
 
 end

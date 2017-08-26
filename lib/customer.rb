357b9fefd4a2f385912d08743d811f6d4b2792ca
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2017-08-26 09:29:45 -0700

    add class method find

--- lib/customer.rb
+++ lib/customer.rb
@@ -2,21 +2,24 @@ require 'csv'
 
 module Grocery
   class Customer
-    attr_reader :id, :email, :delivery_address, :all_info
+    attr_reader :id, :email, :delivery_address, :all_info, :customer_information
 
     def initialize(id, email, delivery_address)
       @id = id
       @email = email
       @delivery_address = delivery_address
+  ### Is it necessary to add this customer information component here? Do I need
+  ### to store this in an array? Can it be called as a whole without doing that?
+      @customer_information = [@id, @email, @delivery_information]
       @all_info = "The new customer has an id of #{@id}, email: #{@email}, and their address is #{@delivery_address}."
     end
 
-    def print_info(array)
-      array.each do |line|
+    def print_info
+      # array.each do |line|
         puts "Customer ID is #{@id}"
         puts "Customer email is #{@email}"
         puts "Customer address is #{@delivery_address}"
-      end
+      # end
       # return "Id: #{@id} has email #{@email} and their address is #{@address}."
     end
 
@@ -30,7 +33,7 @@ module Grocery
 #disappear and even though they are passed through the customer class initiation
 #they have to be called as instance variables??
       CSV.open("support/customers.csv", "r").each do |line|
-        @id = line[0]
+        @id = line[0].to_i
         # puts "This is the id #{@id}"
         @email = line[1]
         # puts "This is the email #{@email}"
@@ -40,19 +43,52 @@ module Grocery
         all_customers << new_customer
       end
       puts "length is : #{all_customers.length}"
-      all_customers.length.times do |customer|
-        puts line.all_info
+
+      return all_customers
+      # I tried to make it return here, but it wasn't working the way I expected
+      # all_customers.length.times do |customer|
+        # puts line.all_info
         # puts "Customer ID: #{@id} **** Customer email:#{@email}"
         # puts "Customer address is #{@delivery_address}"
-      end
-#self.all end
-    end
+
+    end #self.all end
+
+    def self.find(customer_num)
+      ## multiple ways to acquire the same information - comes down to user desing
+      ## do we want to be able to input a number or do we want to write it out in the
+      ## code at the bottom or as an argument in the class method 
+      # puts "Enter the ID of the customer you are looking for."
+      # customer_num = gets.chomp.to_i
+
+      new_customer = nil
+      CSV.open("support/customers.csv", "r").each do |line|
+        if customer_num == line[0].to_i
+          @id = line[0].to_i
+          # puts "This is the id #{@id}"
+          @email = line[1]
+          # puts "This is the email #{@email}"
+          @delivery_address = line[2..5]
+          # puts "this is the delivery address : #{@delivery_address}"
+          new_customer = Customer.new(@id, @email, @delivery_address)
+        end
+      end #each loop end
+
+      return new_customer.print_info
+
+    end #self.find end
 
 
-#class end
-  end
-#module end
+  end #class end
+
+end #module end
+
+
+puts frenchie = Grocery::Customer.all
+
+###### I am surprised this worked!!####
+frenchie.each do |line|
+  print line.print_info
 end
 
-
-frenchie = Grocery::Customer.all
+hello = Grocery::Customer.find(3)
+puts hello

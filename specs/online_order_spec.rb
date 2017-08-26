1272421947beb1228d85f919e31ed6bfa8471414
Bennett Rahn <rahnibennett@gmail.com>
2017-08-25 21:56:29 -0700

    add/update .all method tests and add another for initialize to check that pending is the default status.

--- specs/online_order_spec.rb
+++ specs/online_order_spec.rb
@@ -2,20 +2,14 @@
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
+require 'minitest/pride'
 
-# TODO: uncomment the next line once you start wave 3
 require_relative '../lib/online_order'
 require_relative '../lib/order'
-# You may also need to require other classes here
-
-# Because an OnlineOrder is a kind of Order, and we've
-# already tested a bunch of functionality on Order,
-# we effectively get all that testing for free! Here we'll
-# only test things that are different.
 
 describe "OnlineOrder" do
   before do
-    @online_order = Grocery::OnlineOrder.new(1, {"Lobster" => 17.18, "Annatto seed" => 58.38, "Camomile" => 83.21}, 25, :completed)
+    @online_order = Grocery::OnlineOrder.new(1, {"Lobster" => 17.18, "Annatto seed" => 58.38, "Camomile" => 83.21}, 25, :complete)
 
 
   end
@@ -23,60 +17,76 @@ describe "OnlineOrder" do
     it "Is a kind of Order" do
       # Check that an OnlineOrder is in fact a kind of Order
       @online_order.must_be_kind_of Grocery::Order
-      # Instatiate your OnlineOrder here
-      # online_order =
-      # online_order.must_be_kind_of Grocery::Order
     end
 
     it "Can access Customer object" do
-      # TODO: Your test code here!
-      @online_order.customer(22).must_equal Grocery::Customer.find(22)
+      @online_order.customer.must_equal Grocery::Customer.find(25)
     end
 
     it "Can access the online order status" do
-      # TODO: Your test code here!
       @online_order.must_respond_to :status
+    end
+
+    it "Pending is default status" do
+      statusless = Grocery::OnlineOrder.new(1, {"Lobster" => 17.18, "Annatto seed" => 58.38, "Camomile" => 83.21}, 25)
+
+      statusless.status.must_equal :pending
     end
   end
 
   describe "#total" do
     it "Adds a shipping fee" do
         order = Grocery::Order.new(1, {"Lobster" => 17.18, "Annatto seed" => 58.38, "Camomile" => 83.21})
-      # TODO: Your test code here!
       @online_order.total.must_equal (order.total + 10)
       # (180.68)
     end
 
     it "Doesn't add a shipping fee if there are no products" do
-      product_free = Grocery::OnlineOrder.new(35, {}, 25, :completed)
-      # TODO: Your test code here!
+      product_free = Grocery::OnlineOrder.new(35, {}, 25, :complete)
       product_free.total.must_equal 0
     end
   end
 
   describe "#add_product" do
-    it "Does not permit action for processing, shipped or completed statuses" do
-      # TODO: Your test code here!
+    it "Does not permit action for processing, shipped or complete statuses" do
+      ##NEED TO ADD AN ARGUMENTERROR HERE!!
       @online_order.add_product("Kale", 2.99).must_equal false
+      @online_order.products.length.must_equal 3
     end
 
     it "Permits action for pending and paid satuses" do
       pending = Grocery::OnlineOrder.new(1, {"Lobster" => 17.18, "Annatto seed" => 58.38, "Camomile" => 83.21}, 25, :paid)
-      # TODO: Your test code here!
       pending.add_product("Kale", 2.99).must_equal true
+      pending.products.length.must_equal 4
     end
   end
 
-  xdescribe "OnlineOrder.all" do
-    it "Returns an array of all online orders" do
-      # TODO: Your test code here!
-      # Useful checks might include:
-      #   - OnlineOrder.all returns an array
-      #   - Everything in the array is an Order
-      #   - The number of orders is correct
-      #   - The customer is present
-      #   - The status is present
-      # Feel free to split this into multiple tests if needed
+  describe "OnlineOrder.all" do
+    before do
+      @online_orders = Grocery::OnlineOrder.all
+    end
+
+    it "Returns an array" do
+      @online_orders.must_be_kind_of Array
+    end
+
+    it "Array is filled with Orders" do
+      10.times do
+        @online_orders[rand(100)].must_be_kind_of Grocery::Order
+      end
+    end
+
+    it "Has right number of items" do
+      @online_orders.length.must_equal 100
+    end
+
+    it "The customer is present" do
+      customer = @online_orders[0].customer
+      customer.must_equal Grocery::Customer.find(customer.id)
+    end
+
+    it "the status is present" do
+      @online_orders[0].status.must_equal :complete
     end
   end
 

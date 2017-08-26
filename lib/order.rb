166919baf0b20caa12e6efa61e0a1ff08cc0af31
Eva Sawyer <esawyer25@gmail.com>
2017-08-26 12:04:20 -0700

    Cleaned up code and added comments

--- lib/order.rb
+++ lib/order.rb
@@ -10,43 +10,46 @@ module Grocery
       @products = products
     end
 
+
+    #returns a collection of Order instances, representing all of the Orders described in the CSV. See below for the CSV file specifications
     def self.all
+      #csv format = order_id,item1:price1;item2:price2...
+      #the number of item and price pairs can vary.
+      #Goal = (order id, ({item1 => price1}, {item2 => price2}...))
       csv_array_of_all_orders = []
-      #1: Read the csv file
+      #1: Open the csv file and extract order id and all info on products
       CSV.open("support/orders.csv", 'r').each do |line|
-        #1A: divide the cvs file into the order number, and the products
         order_id = line[0]
         order_contents = line[1]
         order_contents = order_contents.split(";")
-        order_contents.to_s.delete("\,")
-        order_contents.to_s.delete(" \" ")
-        #1B. Make hashes for all items in order
-        #ex. {name1 => cost 1}
+        #2. Parse out the item and price of products.
+        #   Store them in a hash.
         products = Hash.new
         item_index = 0
         order_contents.each do |items|
           current_item = order_contents[item_index]
-          item_name = current_item.to_s.split("\:")[0]
-          item_cost = current_item.to_s.split("\:")[1].to_f
+          item = current_item.to_s.split("\:")[0]
+          price = current_item.to_s.split("\:")[1].to_f
           item_index = item_index + 1
-          products[item_name] = item_cost
+          products[item] = price
         end
-        # 1C: make an array with the order id, product hash
-        #ex. (order id, ({name1 => cost 1})
+        # 3: Make an array with the order id and product hash
+        #ex. (order id, ({name1 => cost 1}, ...))
         current_order = [order_id, products]
-        #1D: make an array of the arrays of order id and product hash
+        #4: make an array of the arrays of order id and product hash
         #ex. ((order_id_1, ({name1 => cost 1}),(order_id_2, ({name2 => cost 2}))
         csv_array_of_all_orders << current_order
       end
-      #2. pass the array of arrays of (order id, product hash) into the
-      # Order.new command to make an array of Orders
+      #5. Make a new instance of Order for each object in the array in 4
       collection_of_order_objects = []
       csv_array_of_all_orders.each do |order|
         collection_of_order_objects << Order.new(order[0], order[1])
       end
+      #6. Return an array of all the orders
       return collection_of_order_objects
     end
 
+    #returns an instance of Order where the value of the id field in the CSV matches the passed parameter.
     def self.find(order_id)
       collection_of_orders = Order.all
       collection_of_orders.each do |order|
@@ -57,7 +60,8 @@ module Grocery
       raise ArgumentError.new("Order number #{order_id} could not be found.")
     end
 
-
+    #total calculate cost of the order by: 1. summing the products,
+    #2. adding a 7.5% tax 3. rounding to two decimal places
     def total
       total = 0
       products.each do |name, price|
@@ -66,6 +70,9 @@ module Grocery
       return total.round(2)
     end
 
+    # Add_product takes product name and price.  If the item is not
+    # already present, it and adds the data to the product collection.
+    # Return true if the item is added and false if it is not.
     def add_product(product_name, product_price)
       if @products.key?(product_name)
         return false
@@ -75,6 +82,8 @@ module Grocery
       end
     end
 
+
+    # Remove_product takes a product name and removes the product from the # # collection. Return true if the item was successfully removed and false # if not.
     def remove_product(product_name)
       if @products.key?(product_name)
         @products.delete(product_name)

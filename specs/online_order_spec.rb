9f6aa8042307e68320d9d65f408044cee1fd9461
Sarah Read-Brown <srbusiness2@gmail.com>
2017-08-26 13:01:24 -0700

    Adds tests for online order.all method

--- specs/online_order_spec.rb
+++ specs/online_order_spec.rb
@@ -16,8 +16,12 @@ require_relative '../lib/customer'
 describe "OnlineOrder" do
   before do
     @online_order_one = Grocery::OnlineOrder.new(1,{"Lobster" =>17.18, "Annatto seed" => 58.38, "Camomile" =>83.21}, 25,"complete")
+
     @online_order_two = Grocery::OnlineOrder.new(2,{}, 26,"pending")
+
     @customer_first = Grocery::Customer.new(25, "leonard.rogahn@hagenes.org", {address1: "71596 Eden Route" , city: "Connellymouth" , state: "LA", zip_code: "98872-9105" })
+
+    @online_orders = Grocery::OnlineOrder.all
   end
   describe "#initialize" do
     it "Is a kind of Order" do
@@ -28,8 +32,11 @@ describe "OnlineOrder" do
       @online_order_one.customer_id.must_equal @customer_first.customer_id
     end
 
-    it "Can access the online order status" do
+    it "Can access the online order status, Order ID, products, and customer id" do
       @online_order_one.must_respond_to :status
+      @online_order_one.must_respond_to :id
+      @online_order_one.must_respond_to :customer_id
+      @online_order_one.must_respond_to :products
     end
   end
 
@@ -50,8 +57,6 @@ describe "OnlineOrder" do
       proc{Grocery::OnlineOrder.new(1,{"Lobster" =>17.18, "Annatto seed" => 58.38, "Camomile" =>83.21}, 25,"shipped").add_product("Macadamia Nut",79.19)}.must_raise ArgumentError
 
       proc{Grocery::OnlineOrder.new(1,{"Lobster" =>17.18, "Annatto seed" => 58.38, "Camomile" =>83.21}, 25,"complete").add_product("Macadamia Nut",79.19)}.must_raise ArgumentError
-
-
     end
 
     it "Permits action for pending and paid satuses" do
@@ -74,16 +79,37 @@ describe "OnlineOrder" do
     end
   end
 
-  xdescribe "OnlineOrder.all" do
-    it "Returns an array of all online orders" do
-      # TODO: Your test code here!
-      # Useful checks might include:
-      #   - OnlineOrder.all returns an array
-      #   - Everything in the array is an Order
-      #   - The number of orders is correct
-      #   - The customer is present
-      #   - The status is present
-      # Feel free to split this into multiple tests if needed
+
+  describe "OnlineOrder.all" do
+
+    it "Everything in the array is an Order" do
+      @online_orders.each do |order|
+        order.must_be_kind_of Grocery::OnlineOrder
+      end
+    end
+
+    it "OnlineOrder.all returns an array" do
+      @online_orders.must_be_kind_of Array
+    end
+
+    it "The number of online orders is correct (100)" do
+      @online_orders.length.must_equal 100
+    end
+
+    #The wording on this prompt is vauge. I'm not sure this test is targeted at extracting the approptiate information
+    it "The customer is present - Raises and error if the customer ID doesn't exist" do
+      # proc{Grocery::OnlineOrder.find(107)}.must_raise ArgumentError
+    end
+
+    it "OnlineOrder.all responds to status" do
+      @online_orders.must_respond_to :status
+
+    end
+
+    it "The status is present - all users have a status AKA default parameter value works" do
+      @online_orders.each do |order|
+        order.status.wont_be_empty
+      end
     end
   end
 
@@ -91,5 +117,5 @@ describe "OnlineOrder" do
     it "Returns an array of online orders for a specific customer ID" do
       # TODO: Your test code here!
     end
-  end
-end
+  end #end of describe "OnlineOrder.find_by_customer"
+end #end of describe "OnlineOrder"

3dc8d34da99690877e285430c265273d102ae3fd
Eva Sawyer <esawyer25@gmail.com>
2017-08-25 21:15:54 -0700

    added tests for OnlineOrder.find_by_customer

--- specs/online_order_spec.rb
+++ specs/online_order_spec.rb
@@ -91,26 +91,91 @@ describe "OnlineOrder" do
       updated_products ={ "banana" => 1.99, "cracker" => 3.00, "apple" => 3.00}
 
       online_order.add_product("apple", 3.00)
-      online_order.products.must_equal products
+      online_order.products.must_equal updated_products
     end
   end
 
   describe "OnlineOrder.all" do
-    it "Returns an array of all online orders" do
-      # TODO: Your test code here!
-      # Useful checks might include:
-      #   - OnlineOrder.all returns an array
-      #   - Everything in the array is an Order
-      #   - The number of orders is correct
-      #   - The customer is present
-      #   - The status is present
-      # Feel free to split this into multiple tests if needed
+    it "Returns an array" do
+      all_online_orders =Grocery::OnlineOrder.all
+      all_online_orders.must_be_kind_of Array
+    end
+
+    #Note, think about changing other similar methods to a each loop
+    it "Everything in the array is an Order" do
+      all_online_orders =Grocery::OnlineOrder.all
+      all_online_orders.each do |single_order|
+        single_order.must_be_kind_of Grocery::Order
+      end
+    end
+
+    it "The number of orders in the array equals the number in the csv file" do
+      all_online_orders =Grocery::OnlineOrder.all
+      csv_length = CSV.read("support/online_orders.csv", 'r').length
+      all_online_orders.length.must_equal csv_length
+    end
+
+    it "You can access the Customer class and it's methods" do
+      all_online_orders =Grocery::OnlineOrder.all
+      all_online_orders.each do |one_online_order|
+        one_online_order.must_respond_to :customer
+        one_online_order.customer.must_respond_to :customer_id
+        one_online_order.customer.must_respond_to :email
+        one_online_order.customer.must_respond_to :address
+      end
+    end
+
+    it "The status is present" do
+      all_online_orders =Grocery::OnlineOrder.all
+      all_online_orders.each do |one_online_order|
+        one_online_order.must_respond_to :status
+      end
     end
   end
 
   describe "OnlineOrder.find_by_customer" do
-    it "Returns an array of online orders for a specific customer ID" do
-      # TODO: Your test code here!
+    #it "Returns an array of online orders for a specific customer ID" do
+    # TODO: Your test code here!
+    it "Returns an array" do
+      online_orders = Grocery::OnlineOrder.find_by_customer(14)
+      online_orders.must_be_kind_of Array
+    end
+
+    it "The objects in the array are orders" do
+      online_orders = Grocery::OnlineOrder.find_by_customer(14)
+      online_orders.each do |order|
+        order.must_be_kind_of Grocery::Order
+      end
+
+    end
+
+    it "Can find the all the orders from a single customer" do
+      online_orders = Grocery::OnlineOrder.find_by_customer(14)
+
+      find_orders =[]
+      online_orders.each do |order|
+         find_orders << order.products
+      end
+      find_orders = find_orders.join
+      find_orders = find_orders.delete("{").delete("}").delete("\"").delete("=>")
+      find_orders
+
+      csv = CSV.open("support/online_orders.csv", 'r')
+      csv_orders = []
+      csv.each do |line|
+        if line[-2].to_i == 14
+          csv_orders << line[1...-2]
+        end
+      end
+      puts ""
+      csv_orders = csv_orders.join.delete(":")
+      csv_orders = csv_orders.gsub(/([;])/, ", ")
+
+          find_orders.must_equal csv_orders
+    end
+
+    it "Raises an error if the customer did not place and online order" do
+      proc {Grocery::OnlineOrder.find_by_customer(1000)}.must_raise ArgumentError
     end
   end
 end

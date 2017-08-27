95adf60c5e65d381d3c8f3fc19d904cae855b874
Sara C <srachandler@gmail.com>
2017-08-26 19:39:14 -0700

    passed Customer.all test

--- lib/customer.rb
+++ lib/customer.rb
@@ -14,7 +14,7 @@ module Grocery
      CSV.read('./support/customers.csv').each do |row|
        id = row.delete_at(0).to_i
        email = row.delete_at(0)
-       address_string = row.join
+       address_string = row.join(" ")
        address_array = address_string.split(',')
        address_string = address_array.join(" ")
        address = address_string

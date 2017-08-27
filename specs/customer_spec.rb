4b5aa0e6e130bee4f2b4d99bfdd9cf893162c259
Sarah Read-Brown <srbusiness2@gmail.com>
2017-08-27 12:36:46 -0700

    Dry up customer spec

--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -3,20 +3,16 @@ require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require 'pry'
 require_relative '../lib/customer'
-# TODO: uncomment the next line once you start wave 3
-# require_relative '../lib/customer'
-# 1,leonard.rogahn@hagenes.org,71596 Eden Route,Connellymouth,LA,98872-9105
+
 describe "Customer" do
   before do
     @customer_first = Grocery::Customer.new(1, "leonard.rogahn@hagenes.org", {address1: "71596 Eden Route" , city: "Connellymouth" , state: "LA", zip_code: "98872-9105" })
     @customer_last = Grocery::Customer.new(35, "rogers_koelpin@oconnell.org", {address1: "7513 Kaylee Summit" , city: "Uptonhaven" , state: "DE", zip_code: "64529-2614" })
+    @customers = Grocery::Customer.all
   end
   describe "#initialize" do
-    #"leonard.rogahn@hagenes.org", "71596 Eden Route,Connellymouth,LA,98872-9105")
-    # {address1: , city: , state: , zipcode: }
-    it "Takes an ID, email and address info" do
-      # customer = Grocery::Customer.new(1, "leonard.rogahn@hagenes.org", {address1: "71596 Eden Route" , city: "Connellymouth" , state: "LA", zip_code: "98872-9105" })
 
+    it "Takes an ID, email and address info" do
       address = {:address1=>"71596 Eden Route", :city=>"Connellymouth",:state=>"LA", :zip_code=>"98872-9105"}
 
       @customer_first.must_respond_to :customer_id && :email && :address
@@ -28,26 +24,26 @@ describe "Customer" do
 
   describe "Customer.all" do
     it "Customer.all returns an array" do
-      Grocery::Customer.all.must_be_kind_of Array
+      @customers.must_be_kind_of Array
     end
 
     it "Everything in the array is of class customer" do
-      Grocery::Customer.all.each do |customer|
+      @customers.each do |customer|
         customer.must_be_kind_of Grocery::Customer
       end
     end
 
     it "Customer.all returns and array with the correct number of customers" do
-      Grocery::Customer.all.length.must_equal 35
+      @customers.length.must_equal 35
     end
 
     it "the ID email and address customers.all[0] and customers.all[35] match what is in the CSV file" do
-      Grocery::Customer.all.first.customer_id.must_equal @customer_first.customer_id
-      Grocery::Customer.all.first.address.must_equal @customer_first.address
-      Grocery::Customer.all.first.email.must_equal @customer_first.email
-      Grocery::Customer.all.last.customer_id.must_equal @customer_last.customer_id
-      Grocery::Customer.all.last.address.must_equal @customer_last.address
-      Grocery::Customer.all.last.email.must_equal @customer_last.email
+      @customers.first.customer_id.must_equal @customer_first.customer_id
+      @customers.first.address.must_equal @customer_first.address
+      @customers.first.email.must_equal @customer_first.email
+      @customers.last.customer_id.must_equal @customer_last.customer_id
+      @customers.last.address.must_equal @customer_last.address
+      @customers.last.email.must_equal @customer_last.email
     end
   end
 

ba260f0b825b0e0a0d4974fdab5d6b6e5c845e8b
nkiru onwuneme <generalhrh@gmail.com>
2017-08-27 10:18:00 -0700

    Edited class def

--- lib/online_order.rb
+++ lib/online_order.rb
@@ -1,25 +1,27 @@
 require 'csv'
 require_relative 'order'
 
-class OnlineOrder < Order
+module Grocery
+  class OnlineOrder < Order
 
-  def initialize
-    super
-    @status = :status
-    @customer = Customer.new
-  end
+    # def initialize
+    #   super
+    #   @status = :status
+    #   @customer = Customer.new
+    # end
+    #
+    # def total
+    #
+    # end
+    #
+    # def self.all
+    # end
+    #
+    # def self.find(id)
+    # end
+    #
+    # def self.find_by_customer(customer_id)
+    # end
 
-  def total
-
-  end
-
-  def self.all
-  end
-
-  def self.find(id)
-  end
-
-  def self.find_by_customer(customer_id)
-  end
-
-end #end of class
+  end #end of class
+end #end of module

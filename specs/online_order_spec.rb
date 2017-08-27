abd3afcefe8405c8bedfdfcaaa5ea552cc1f1835
nkiru onwuneme <generalhrh@gmail.com>
2017-08-27 06:17:50 -0700

    Added test1

--- specs/online_order_spec.rb
+++ specs/online_order_spec.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
+require_relative '../lib/online_order'
 
 # TODO: uncomment the next line once you start wave 3
 # require_relative '../lib/online_order'
@@ -11,61 +12,68 @@ require 'minitest/skip_dsl'
 # we effectively get all that testing for free! Here we'll
 # only test things that are different.
 
-xdescribe "OnlineOrder" do
+describe "OnlineOrder" do
   describe "#initialize" do
     it "Is a kind of Order" do
+      # id = 23
+      # products = {Lobster: 17.18, Annattoseed: 58.38,  Camomile: 83.21}
+      #
+      # order = Grocery::Order.new(id, products)
       # Check that an OnlineOrder is in fact a kind of Order
 
       # Instatiate your OnlineOrder here
-      # online_order =
-      # online_order.must_be_kind_of Grocery::Order
-    end
-
-    it "Can access Customer object" do
-      # TODO: Your test code here!
-    end
-
-    it "Can access the online order status" do
-      # TODO: Your test code here!
-    end
-  end
-
-  describe "#total" do
-    it "Adds a shipping fee" do
-      # TODO: Your test code here!
-    end
-
-    it "Doesn't add a shipping fee if there are no products" do
-      # TODO: Your test code here!
-    end
-  end
-
-  describe "#add_product" do
-    it "Does not permit action for processing, shipped or completed statuses" do
-      # TODO: Your test code here!
-    end
-
-    it "Permits action for pending and paid satuses" do
-      # TODO: Your test code here!
-    end
-  end
-
-  describe "OnlineOrder.all" do
-    it "Returns an array of all online orders" do
-      # TODO: Your test code here!
-      # Useful checks might include:
-      #   - OnlineOrder.all returns an array
-      #   - Everything in the array is an Order
-      #   - The number of orders is correct
-      #   - The customer is present
-      #   - The status is present
-      # Feel free to split this into multiple tests if needed
-    end
-  end
-  
-  describe "OnlineOrder.find_by_customer" do
-    it "Returns an array of online orders for a specific customer ID" do
-      # TODO: Your test code here!
+      online_order = OnlineOrder.new(order)
+      puts online_order
+      online_order.must_be_kind_of Order
     end
   end
 end
+#
+  #   it "Can access Customer object" do
+  #     # TODO: Your test code here!
+  #   end
+  #
+  #   it "Can access the online order status" do
+  #     # TODO: Your test code here!
+  #   end
+  # end
+
+  # describe "#total" do
+  #   it "Adds a shipping fee" do
+  #     # TODO: Your test code here!
+  #   end
+  #
+  #   it "Doesn't add a shipping fee if there are no products" do
+  #     # TODO: Your test code here!
+  #   end
+  # end
+  #
+  # describe "#add_product" do
+  #   it "Does not permit action for processing, shipped or completed statuses" do
+  #     # TODO: Your test code here!
+  #   end
+  #
+  #   it "Permits action for pending and paid satuses" do
+  #     # TODO: Your test code here!
+  #   end
+  # end
+  #
+  # describe "OnlineOrder.all" do
+  #   it "Returns an array of all online orders" do
+  #     # TODO: Your test code here!
+  #     # Useful checks might include:
+  #     #   - OnlineOrder.all returns an array
+  #     #   - Everything in the array is an Order
+  #     #   - The number of orders is correct
+  #     #   - The customer is present
+  #     #   - The status is present
+  #     # Feel free to split this into multiple tests if needed
+  #   end
+  # end
+  #
+  # describe "OnlineOrder.find_by_customer" do
+  #   it "Returns an array of online orders for a specific customer ID" do
+  #     # TODO: Your test code here!
+  #   end
+  # end
+# end

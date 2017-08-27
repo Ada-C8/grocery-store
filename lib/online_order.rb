76b98b62f68e7ca86abd1ebaa60a02a08108ad00
Jocelyn Gonzalez <jocegonz@gmail.com>
2017-08-27 11:37:38 -0700

    complete reqs for OnlineOrder.all

--- lib/online_order.rb
+++ lib/online_order.rb
@@ -8,12 +8,14 @@ class OnlineOrder < Order
   attr_reader :id, :products, :customer_id, :status
   STATUS = [:pending, :paid, :processing, :shipped, :complete]
 
-  def initialize(id, products, customer_id, status = pending)
+  def initialize(id, products, customer_id, status = "pending")
 
     @id = id
     @products = products
     @customer_id = Grocery::Customer.find(customer_id.to_i)
-    @status = status.to_sym
+
+    #change to sym but doesn't set default as pending
+    @status = status
 
     #add customer object
     #add fulfillment status, stored as Symbol
@@ -24,22 +26,20 @@ class OnlineOrder < Order
     # :complete
   end
 
+  def self.all
+    #returns OnlineOrder instances
+    #represents all OnlineOrders from CSV
+    online_order_array = []
+    CSV.open('../support/online_orders.csv', 'r').each do |line|
+      id = line.shift.to_i
+      products = line.shift
+      customer_id = line.shift
+      status = line #status is converted to symbol
+      online_order_array << Grocery::OnlineOrder.new(id, products, customer_id, status)
+    end
+    return online_order_array
+  end
 
-
-  # def self.all
-  #   #returns OnlineOrder instances
-  #   #represents all OnlineOrders from CSV
-  #   online_order_array = []
-  #   CSV.open('../support/online_orders.csv', 'r').each do |line|    ##
-  #     id = line.shift.to_i
-  #     products = line.shift
-  #     customer_id = line.shift
-  #     status = line #status is converted to symbol
-  #     online_order_array << Order::OnlineOrders.new(id, products, customer_id, status)
-  #   end
-  #   return online_order_array
-  # end
-  #
   # def self.find
   #
   # end
@@ -47,10 +47,11 @@ class OnlineOrder < Order
   # def self.find_by_customer(customer_id)
   # end
   #
-  # def total
-  #   super
-  #   #add $10 shipping
-  # end
+  def total
+    super
+    #add $10 shipping
+
+  end
   #
   # def add_product
   #   #QUOTE "should be updated to permit...if"
@@ -60,3 +61,6 @@ class OnlineOrder < Order
 
 end
 end #end module
+
+all_online_orders = Grocery::OnlineOrder.all
+puts all_online_orders

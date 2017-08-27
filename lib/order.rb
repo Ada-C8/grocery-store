bf0ba922379d5e2ca7daa869d30846a1fbbc7c20
Jan Edrozo <jedrozo@gmail.com>
2017-08-26 23:53:18 -0700

    file pathway update

--- lib/order.rb
+++ lib/order.rb
@@ -63,10 +63,10 @@ module Grocery
     #   @products.delete(product_name)
     # end
 
-    def self.all(file_pathway)
+    def self.all
       # This class method will handle all of the fields from the CSV file used as input
       # To test, choose the data from the first line of the CSV file and ensure you can create a new instance of your Order using that data
-      orders = CSV.read(file_pathway, converters: :numeric)
+      orders = CSV.read('support/orders.csv', converters: :numeric)
 
       all_orders = []
       orders.each do |row|
@@ -107,53 +107,7 @@ module Grocery
 
   end#of_Order_class
 
-  class Customer
-    attr_reader :customer_id, :email_address, :delivery_address
-
-    def initialize(customer_id, email_address, delivery_address)
-      @customer_id = customer_id
-      @email_address = email_address
-      @delivery_address = delivery_address
-    end
-
-    def self.all
-      customers = CSV.read('/Users/janedrozo/Desktop/grocery-store/support/customers.csv')
-
-      all_customers = []
-
-      customers.each do |row|
-        customer_id = row[0]
-        email_address = row[1]
-
-        #SHORTHAND for delivery_address:
-        delivery_address = (row[2..5]).join(",")
-        #LONGHAND for delivery_address:
-        # delivery_address = "#{row[2]},#{row[3]},#{row[4]},#{row[5]}"
-
-        all_customers << Customer.new(customer_id.to_i, email_address, delivery_address)
-      end
-
-      return all_customers
-    end
-
-    def self.find(customer_id)
-      customers = Grocery::Customer.all
-
-      customers.each do |customer_row_info|
-        if customer_row_info.customer_id == customer_id
-          return customer_row_info
-        end
-      end
-      raise ArgumentError.new("CUSTOMER ##{customer_id} NOT FOUND!")
-    end
-  end#of_Customer_class
-
 end#of_module
 
 #TEST ORDER
-# ap Grocery::Order.all('/Users/janedrozo/Desktop/grocery-store/support/orders.csv')
-
-#TEST CUSTOMER
-# ap Grocery::Customer.all
-
-ap Grocery::Customer.find(1)
+ap Grocery::Order.all

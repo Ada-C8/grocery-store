e6af849207734a7e0e377772763019af4b6be1ab
Sara C <srachandler@gmail.com>
2017-08-26 19:03:35 -0700

    Added self.all method

---
+++ lib/customer.rb
@@ -0,0 +1,60 @@
+require 'csv'
+module Grocery
+   class Customer
+
+   def initialize(id, email, address)
+     @id = id
+     @email = email
+     @address = address
+   end
+
+   def self.all
+     customers = []
+     CSV.read('./support/customers.csv').each do |row|
+       id = row.delete_at(0).to_i
+       email = row.delete_at(0)
+       address_string = row.join
+       address_array = address_string.split(',')
+       address_string = address_array.join(" ")
+       address = address_string
+       customers << self.new(id, email, address)
+     end
+     return customers
+   end
+
+  
+
+
+
+
+
+
+
+
+   end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  end
+end

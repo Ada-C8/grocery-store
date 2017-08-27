1c0213f41a078ea0be494f0056c7483e03bb971d
Maria McGrew <mcgmar1@outlook.com>
2017-08-26 23:16:17 -0700

    customer.all tests passing

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,13 +1,31 @@
 
 module Grocery
   class Customer
-    attr_reader :customer_id, :email, :address
+    attr_reader :customer_id, :email, :address, :city, :state, :zip
 
     def initialize(customer_id, email, address)
       @customer_id = customer_id
       @email = email
       @address = address
     end
+
+    def self.all
+      @all_customers = []
+
+      CSV.read("./support/customers.csv").each do |row|
+        customer_id = row[0].to_i
+        email = row[1].to_s
+        address = row[2].to_s
+        city = row[3].to_s
+        state = row[4].to_s
+        zip = row[5].to_s
+
+        customer = Customer.new(customer_id, email, address)
+        @all_customers << customer
+      end
+      return @all_customers
+    end
+
 
   end #end of class
 
--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -22,23 +22,35 @@ describe "Customer" do
       customer.address.must_equal address
       customer.address.must_be_kind_of String
     end #end of it
-
     end #end of describe 2
 
 
-  # describe "Customer.all" do
-  #   it "Returns an array of all customers" do
-  #     # TODO: Your test code here!
-  #     # Useful checks might include:
-  #     #   - Customer.all returns an array
-  #     #   - Everything in the array is a Customer
-  #     #   - The number of orders is correct
-  #     #   - The ID, email address of the first and last
-  #     #       customer match what's in the CSV file
-  #     # Feel free to split this into multiple tests if needed
-  #   end
-  # end
-  #
+  describe "Customer.all" do
+    it "Returns an array of all customers" do
+      Grocery::Customer.all.must_be_kind_of Array
+    end
+
+    it "Everything in the array is a Customer" do
+      Grocery::Customer.all.each do |customer|
+        customer.must_be_kind_of Grocery::Customer
+      end
+    end
+
+    it "the number of customers is correct" do
+      Grocery::Customer.all.length.must_equal 35
+    end
+
+    it "the customer_id, email, address of the first and last customer matches the CSV file" do
+      Grocery::Customer.all.first.customer_id.must_equal 1
+      Grocery::Customer.all.first.email.must_equal "leonard.rogahn@hagenes.org"
+      Grocery::Customer.all.first.address.must_equal "71596 Eden Route"
+
+      Grocery::Customer.all.last.customer_id.must_equal 35
+      Grocery::Customer.all.last.email.must_equal "rogers_koelpin@oconnell.org"
+      Grocery::Customer.all.last.address.must_equal "7513 Kaylee Summit"
+    end
+  end
+
   # describe "Customer.find" do
   #   it "Can find the first customer from the CSV" do
   #     # TODO: Your test code here!

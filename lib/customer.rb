2a307d2589f7ebd1095dd9fc83b8cd7332bd24ae
nkiru onwuneme <generalhrh@gmail.com>
2017-08-28 04:20:55 -0700

    Re-commit changes

--- lib/customer.rb
+++ lib/customer.rb
@@ -15,7 +15,7 @@ module Grocery
       CSV.open("./support/customers.csv", "r").each do |line|
         id = line[0].to_i
         email_address = line[1]
-        delivery_address =  line[2] + ", " + line[3] + ", " + line[4] + "" + line[5]
+        delivery_address =  line[2] + ", " + line[3] + ", " + line[4] + " " + line[5]
 
         customers_info << Grocery::Customer.new(id, email_address, delivery_address)
       end
@@ -23,7 +23,6 @@ module Grocery
     end
 
     def self.find(id)
-      # returns an instance of Customer where the value of the id field in the CSV matches the passed parameter
       found_customer = nil
       customers = self.all
       customers.each do |customer|
@@ -35,28 +34,3 @@ module Grocery
     end
   end
 end
-
-
-
-
-
-
-#######################
-# testing
-# customer_info = []
-# CSV.open("../support/customers.csv", "r").each do |line|
-#   id = line[0].to_i #variable stores all the numbers, object of all id numbers?
-#   # puts id
-#   # print line
-#   email_address = line[1]
-#   # puts email_address
-#
-# delivery_address =  line[2] + ", " + line[3] + ", " + line[4] + "" + line[5]
-#
-# customer_info.push(id, email_address, delivery_address)
-# # puts delivery_address
-# end
-#
-# puts customer_info
- #  d = a + ", " + b + " " + c
- # => "71596 Eden Route, Connellymouth LA"

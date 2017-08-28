8eaca4a078e8fc31bb880a57ae4366b01fe77c74
Jocelyn Gonzalez <jocegonz@gmail.com>
2017-08-27 17:22:02 -0700

    complete find_by_customer for OnlineOrder

--- lib/online_order.rb
+++ lib/online_order.rb
@@ -9,13 +9,11 @@ class OnlineOrder < Order
   STATUS = [:pending, :paid, :processing, :shipped, :complete]
 
   def initialize(id, products, customer_id, status = "pending")
-
-    @id = id
+    @id = id.to_i #removed to_i here
     @products = products
-    @customer_id = Grocery::Customer.find(customer_id.to_i)
-
+    @customer_id = Grocery::Customer.find(customer_id.to_i) #removed to_i here
     #change to sym but doesn't set default as pending
-    @status = status
+    @status = status.to_sym
 
     #add customer object
     #add fulfillment status, stored as Symbol
@@ -31,27 +29,57 @@ class OnlineOrder < Order
     #represents all OnlineOrders from CSV
     online_order_array = []
     CSV.open('../support/online_orders.csv', 'r').each do |line|
-      id = line.shift.to_i
-      products = line.shift
+      id = line.shift # problems using .to_i
+      products = {}
+      produce = line.shift.split(";")
+      produce.each do |items|
+        two_item_array = items.split(":")
+        item = two_item_array[0]
+        price = two_item_array[1]
+        products[item] = price
+        end
       customer_id = line.shift
-      status = line #status is converted to symbol
+      status = line.join() #status should be converted to symbol in initialize
       online_order_array << Grocery::OnlineOrder.new(id, products, customer_id, status)
     end
     return online_order_array
   end
 
-  # def self.find
-  #
-  # end
-  #
-  # def self.find_by_customer(customer_id)
-  # end
-  #
-  def total
+  def self.find(id)
     super
-    #add $10 shipping
-
+#     #all_ids = [] #not currently using
+#     all_online_orders = Grocery::OnlineOrder.all
+#     all_online_orders.each do |arr|
+#       if arr.id == id
+#         return "id: #{arr.id} #{arr.id.class},
+# products: #{arr.products} #{arr.products.class},
+# customer id: #{arr.customer_id} #{arr.customer_id.class},
+# status: #{arr.status} #{arr.status.class}"
+#         ##The following errors out and wants to remove to_i, to_sym in initialize
+#         # return Grocery::OnlineOrder.new(arr.id, arr.products, arr.customer_id, arr.status)
+#       end
+#     end
+#   #  raise ArgumentError.new("Invalid parameters for order number")
   end
+
+  def self.find_by_customer(customer_id)
+    customer_array = []
+    online_orders = Grocery::OnlineOrder.all
+    online_orders.each do |o_order|
+      if o_order.customer_id.id == customer_id
+        customer_array << o_order
+      end
+    end
+    
+    return customer_array
+  end
+
+  #
+  # def total
+  #   super
+  #   #add $10 shipping
+  #
+  # end
   #
   # def add_product
   #   #QUOTE "should be updated to permit...if"
@@ -61,6 +89,3 @@ class OnlineOrder < Order
 
 end
 end #end module
-
-all_online_orders = Grocery::OnlineOrder.all
-puts all_online_orders

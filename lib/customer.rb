86fe37278c9e6e803ef43dc2f3dce32a917a2a3c
Amy Cash <amymcash@gmail.com>
2017-08-28 08:02:20 -0700

    Final

--- lib/customer.rb
+++ lib/customer.rb
@@ -15,8 +15,6 @@ module Grocery
       @delivery_address = delivery_address
     end
 
-    #   # self.all - returns a collection of Customer instances, representing all of the Customer described in the CSV. See below for the CSV file specifications
-
     def self.all
 
       @@all_customers = []
@@ -37,7 +35,6 @@ module Grocery
     end
 
     def self.find(id)
-      # self.find(id) - returns an instance of Order where the value of the id field in the CSV matches the passed parameter.
       customers = Grocery::Customer.all
       item_num = []
 
@@ -53,27 +50,3 @@ module Grocery
     end# method def
   end #customer class
 end #module
-
-puts Grocery::Customer.find(10).inspect
-puts Grocery::Customer.find(350)
-
-
-#def self.find(id)
-#   CSV.open('../support/customers.csv', 'r').each do |line|
-#     if id.to_i == line[0].to_i #begin if statement
-#       line[0] = line[0].to_i
-#       line[2] = line[2] + ", " + line[3] + ", " + line[4] + ", " + line[5]
-#       line.delete_at(5)
-#       line.delete_at(4)
-#       line.delete_at(3)
-#       return Customer.new(line[0], line[1], line[2])
-#       #break
-#     end # if statement
-#   end #CSV each
-# end# method def
-
-
-# puts Grocery::Customer.find(1).delivery_address
-# puts Grocery::Order.find(10).show_order
-# 1,leonard.rogahn@hagenes.org,71596 Eden Route,Connellymouth,LA,98872-9105
-# 35,rogers_koelpin@oconnell.org,7513 Kaylee Summit,Uptonhaven,DE,64529-2614

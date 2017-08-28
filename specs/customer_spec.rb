84e56b7cd92a28a2ba73c89c9a52dcee8c36c845
Mariana <lmarianarp@gmail.com>
2017-08-28 08:51:15 -0700

    specs

--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -1,41 +1,77 @@
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
+require_relative '../lib/costumer'
+
 
 # TODO: uncomment the next line once you start wave 3
 # require_relative '../lib/customer'
 
-xdescribe "Customer" do
+describe "Customer" do
   describe "#initialize" do
     it "Takes an ID, email and address info" do
-      # TODO: Your test code here!
+
+      id = 123
+      email = "bartsimpson@gmail.com"
+      costumer= Grocery::Costumer.new(id, email, {})
+
+      costumer.must_respond_to :id
+      costumer.id.must_equal id
+      costumer.id.must_be_kind_of Integer
+
+      costumer.must_respond_to :email
+      costumer.email.must_equal email
+      costumer.email.must_be_kind_of String
+
+      costumer.must_respond_to :address
+      costumer.address.length.must_equal 0
     end
   end
 
   describe "Customer.all" do
     it "Returns an array of all customers" do
-      # TODO: Your test code here!
-      # Useful checks might include:
+
+      infopeople = Grocery::Costumer.all
       #   - Customer.all returns an array
+      infopeople.must_be_kind_of Array
       #   - Everything in the array is a Customer
+      infopeople.each do |person|
+        person.must_be_kind_of Grocery::Costumer
+      end
       #   - The number of orders is correct
+      infopeople.length.must_equal  35
       #   - The ID, email address of the first and last
       #       customer match what's in the CSV file
-      # Feel free to split this into multiple tests if needed
+      firstperson = Grocery::Costumer.new(1,"leonard.rogahn@hagenes.org",{"Address 1" => "71596 Eden Route", "City" => "Connellymouth", "State"=> "LA", "Zip-Code" => "98872-9105"})
+      infopeople[0].id.must_equal firstperson.id
+      infopeople[0].email.must_equal firstperson.email
+      infopeople[0].address.must_equal firstperson.address
+
+      lastperson = Grocery::Costumer.new(35,"rogers_koelpin@oconnell.org",{"Address 1" => "7513 Kaylee Summit" ,"City" => "Uptonhaven", "State" => "DE", "Zip-Code" => "64529-2614"})
+      infopeople[-1].id.must_equal lastperson.id
+      infopeople[-1].email.must_equal lastperson.email
+      infopeople[-1].address.must_equal lastperson.address
     end
   end
 
   describe "Customer.find" do
     it "Can find the first customer from the CSV" do
       # TODO: Your test code here!
+      firstperson = Grocery::Costumer.new(1,"leonard.rogahn@hagenes.org",{"Address 1" => "71596 Eden Route", "City" => "Connellymouth", "State"=> "LA", "Zip-Code" => "98872-9105"})
+      Grocery::Costumer.find(1).id.must_equal firstperson.id
+      Grocery::Costumer.find(1).email.must_equal firstperson.email
+      Grocery::Costumer.find(1).address.must_equal firstperson.address
     end
 
     it "Can find the last customer from the CSV" do
-      # TODO: Your test code here!
+      lastperson = Grocery::Costumer.new(35,"rogers_koelpin@oconnell.org",{"Address 1" => "7513 Kaylee Summit" ,"City" => "Uptonhaven", "State" => "DE", "Zip-Code" => "64529-2614"})
+      Grocery::Costumer.find(35).id.must_equal lastperson.id
+      Grocery::Costumer.find(35).email.must_equal lastperson.email
+      Grocery::Costumer.find(35).address.must_equal lastperson.address
     end
 
     it "Raises an error for a customer that doesn't exist" do
-      # TODO: Your test code here!
+      proc{Grocery::Costumer.find(400).must_raise ArgumentError}
     end
   end
 end
--- specs/online_order_spec.rb
+++ specs/online_order_spec.rb
@@ -2,51 +2,112 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 
-# TODO: uncomment the next line once you start wave 3
-# require_relative '../lib/online_order'
-# You may also need to require other classes here
+
+require_relative '../lib/online_order'#online_order
+require_relative '../lib/order'
+require_relative '../lib/costumer'
+
+
 
 # Because an OnlineOrder is a kind of Order, and we've
 # already tested a bunch of functionality on Order,
 # we effectively get all that testing for free! Here we'll
 # only test things that are different.
 
-xdescribe "OnlineOrder" do
+describe "OnlineOrder" do
   describe "#initialize" do
     it "Is a kind of Order" do
       # Check that an OnlineOrder is in fact a kind of Order
 
       # Instatiate your OnlineOrder here
-      # online_order =
-      # online_order.must_be_kind_of Grocery::Order
+      idc = 123
+      email = "bartsimpson@gmail.com"
+      costumer =  Grocery::Costumer.new(idc, email, {})
+      ido = 199
+      online_order = Grocery::OnlineOrder.new(ido, {}, costumer, :pending)
+      online_order.must_be_kind_of Grocery::Order
     end
 
     it "Can access Customer object" do
       # TODO: Your test code here!
+      idc = 123
+      email = "bartsimpson@gmail.com"
+      costumer =  Grocery::Costumer.new(idc, email, {})
+      ido = 199
+      online_order = Grocery::OnlineOrder.new(ido, {}, costumer, :pending)
+      online_order.must_respond_to :costumer
     end
 
     it "Can access the online order status" do
       # TODO: Your test code here!
+      idc = 123
+      email = "bartsimpson@gmail.com"
+      costumer =  Grocery::Costumer.new(idc, email, {})
+      ido = 199
+      online_order = Grocery::OnlineOrder.new(ido, {}, costumer, :pending)
+      online_order.must_respond_to :status
+
     end
   end
 
   describe "#total" do
     it "Adds a shipping fee" do
-      # TODO: Your test code here!
+
+      idc = 123
+      email = "bartsimpson@gmail.com"
+      address = {"Address 1" => "7513 Kaylee Summit" ,"City" => "Uptonhaven", "State" => "DE", "Zip-Code" => "64529-2614"}
+      costumer =  Grocery::Costumer.new(idc, email, address)
+      ido = 1
+      products = { "banana" => 1.99, "cracker" => 3.00 }
+      order = Grocery::Order.new(ido, products)
+      online_order = Grocery::OnlineOrder.new(ido, products, costumer, :pending)
+      online_order.total.must_equal(order.total + 10 )
     end
 
     it "Doesn't add a shipping fee if there are no products" do
-      # TODO: Your test code here!
+
+      idc = 123
+      email = "bartsimpson@gmail.com"
+      address = {"Address 1" => "7513 Kaylee Summit" ,"City" => "Uptonhaven", "State" => "DE", "Zip-Code" => "64529-2614"}
+      costumer =  Grocery::Costumer.new(idc, email, address)
+      ido = 11
+      products = {}
+      order = Grocery::Order.new(ido, products)
+      online_order = Grocery::OnlineOrder.new(ido, products, costumer, :pending)
+      online_order.total.must_equal 0
     end
   end
 
   describe "#add_product" do
     it "Does not permit action for processing, shipped or completed statuses" do
-      # TODO: Your test code here!
+
+      idc = 123
+      email = "bartsimpson@gmail.com"
+      address = {"Address 1" => "7513 Kaylee Summit" ,"City" => "Uptonhaven", "State" => "DE", "Zip-Code" => "64529-2614"}
+      costumer =  Grocery::Costumer.new(idc, email, address)
+      ido = 1
+      products = { "banana" => 1.99, "cracker" => 3.00 }
+      online_order1 = Grocery::OnlineOrder.new(ido, products, costumer, :processing)
+      proc {online_order1.add_product("milk", 2.50).must_raise ArgumentError}
+      online_order2 = Grocery::OnlineOrder.new(ido, products, costumer, :shipped)
+      proc {online_order1.add_product("milk", 2.50).must_raise ArgumentError}
+      online_order3 = Grocery::OnlineOrder.new(ido, products, costumer, :completed)
+      proc {online_order3.add_product("milk", 2.50).must_raise ArgumentError}
+
     end
 
     it "Permits action for pending and paid satuses" do
-      # TODO: Your test code here!
+      idc = 123
+      email = "bartsimpson@gmail.com"
+      address = {"Address 1" => "7513 Kaylee Summit" ,"City" => "Uptonhaven", "State" => "DE", "Zip-Code" => "64529-2614"}
+      costumer =  Grocery::Costumer.new(idc, email, address)
+      ido = 1
+      products = { "banana" => 1.99, "cracker" => 3.00 }
+      online_order1 = Grocery::OnlineOrder.new(ido, products, costumer, :pending)
+      online_order1.add_product("eggs", 2.50).must_equal true
+      online_order2 = Grocery::OnlineOrder.new(ido, products, costumer, :paid)
+      online_order1.add_product("chocolate", 3.23).must_equal true
+
     end
   end
 
@@ -55,14 +116,20 @@ xdescribe "OnlineOrder" do
       # TODO: Your test code here!
       # Useful checks might include:
       #   - OnlineOrder.all returns an array
+      everything = Grocery::Order.all
+      everything.must_be_kind_of Array
       #   - Everything in the array is an Order
+      everything.each do |oneorder|
+        oneorder.must_be_kind_of Grocery::Order
+      end
       #   - The number of orders is correct
+      everything.length.must_equal 100
       #   - The customer is present
       #   - The status is present
       # Feel free to split this into multiple tests if needed
     end
   end
-  
+
   describe "OnlineOrder.find_by_customer" do
     it "Returns an array of online orders for a specific customer ID" do
       # TODO: Your test code here!

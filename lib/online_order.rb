adcde56ec908f0089c7b0a5b1effebaaaac15172
nkiru onwuneme <generalhrh@gmail.com>
2017-08-29 07:03:53 -0700

    Added order method

--- lib/online_order.rb
+++ lib/online_order.rb
@@ -1,6 +1,7 @@
 require 'csv'
 require_relative 'order'
 
+
 module Grocery
   class OnlineOrder < Order
     attr_reader :status, :customer_id, :customer
@@ -8,7 +9,7 @@ module Grocery
     def initialize(id, products, customer_id, status = :pending)
       super(id, products)
       @status = status
-      @customer = Grocery::Customer.find(customer_id) #Customer.new
+      @customer = customer #Grocery::Customer.find(customer_id) #Customer.new
     end
 
     def total
@@ -28,19 +29,53 @@ module Grocery
       end
     end
 
-    # def self.all
-    # end
-    #
-    # def self.find(id)
-    # end
-    #
-    # def self.find_by_customer(customer_id)
-    # end
+    def self.all
+      online_orders = []
+      CSV.open("support/online_orders.csv", 'r').each do |line|
+        products = {}
+        id = line[0].to_i
+        # puts id
+
+        line[1].split(";").each do |product_info|
+          data = product_info.split(":")
+          products[data[0]] = data[1].to_f
+        end
+
+        customer_id = line[2]
+        # print customer_id
+        status = line[3].to_sym
+        puts status
+        online_orders << Grocery::OnlineOrder.new(id, products, customer_id, status)
+      end
+      return online_orders
+    end
+
+    def self.find(id)
+      super(id)
+    end
+
+    def self.find_by_customer(customer_id)
+      customer_orders = []
+      online_orders = self.all
+      if online_orders.any? {|instance| instance.customer.customer_id == customer_id_find.to_s}
+        customers.each do |order|
+          if order.customer.customer.id == customer_id
+            customer_orders << order
+          end
+        end
+      else
+        raise ArgumentError.new("Customer order does not exist")
+      end
+      return customer_orders
+    end
+
 
   end #end of class
 end #end of module
 
-# customers = Grocery::Customer.all
+
+# customer_order = Grocery::OnlineOrder.find_by_customer(25)
+# puts customer_order
 # customer = customers[-1].id
 # print customer
 # online_order = Grocery::OnlineOrder.new(customer, status)
